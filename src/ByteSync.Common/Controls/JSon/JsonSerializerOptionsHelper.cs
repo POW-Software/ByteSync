79bf3912125778b769b228191f013f710c428b72
Paul Fresquet <paul.fresquet@pow-software.com>
2025-01-05 21:01:05 +0100

    refactor: cleanup

--- src/ByteSync.Common/Controls/JSon/JsonSerializerOptionsHelper.cs
+++ src/ByteSync.Common/Controls/JSon/JsonSerializerOptionsHelper.cs
@@ -9,24 +9,21 @@ public static class JsonSerializerOptionsHelper
     {
         var options = new JsonSerializerOptions
         {
-            WriteIndented = true // Active ou désactive l'indentation pour le JSON formaté
+            WriteIndented = true
         };
 
         if (writablePropertiesOnly)
         {
-            // Ignore les propriétés ayant des valeurs par défaut
             options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
         }
 
         if (useUtcDateTimes)
         {
-            // Ajout d'un convertisseur personnalisé pour les DateTime en UTC
             options.Converters.Add(new UtcDateTimeConverter());
         }
 
         if (addTypeNames)
         {
-            // Ajout d'un convertisseur personnalisé pour gérer les noms des types
             options.Converters.Add(new TypeNameHandlingConverter());
         }
         
