26e707eb0706b606745df89e1cfcbd8600972daf
Paul Fresquet <paul.fresquet@pow-software.com>
2025-01-11 08:02:47 +0100

    feat: improve LocalizationService build: improve local.settings.template.json

--- src/ByteSync.Client/Services/Localizations/LocalizationService.cs
+++ src/ByteSync.Client/Services/Localizations/LocalizationService.cs
@@ -9,7 +9,6 @@ using ByteSync.Assets.Resources;
 using ByteSync.Business;
 using ByteSync.Common.Helpers;
 using ByteSync.Interfaces;
-using Splat;
 
 namespace ByteSync.Services.Localizations;
 
@@ -19,14 +18,18 @@ namespace ByteSync.Services.Localizations;
 public class LocalizationService : ILocalizationService
 {
     private readonly IApplicationSettingsRepository _applicationSettingsRepository;
+    private readonly ILogger<LocalizationService> _logger;
     
     private readonly ResourceManager _resourceManager = Resources.ResourceManager;
     
     private readonly BehaviorSubject<CultureDefinition> _cultureDefinition;
 
-    public LocalizationService(IApplicationSettingsRepository? applicationSettingsManager = null)
+
+    
+    public LocalizationService(IApplicationSettingsRepository applicationSettingsManager, ILogger<LocalizationService> logger)
     {
-        _applicationSettingsRepository = applicationSettingsManager ?? Locator.Current.GetService<IApplicationSettingsRepository>()!;
+        _applicationSettingsRepository = applicationSettingsManager;
+        _logger = logger;
             
         AvailableCultures = BuildAvailableCultures();
         
@@ -41,11 +44,6 @@ public class LocalizationService : ILocalizationService
     
     public CultureDefinition CurrentCultureDefinition => _cultureDefinition.Value;
     
-    // public CultureInfo CurrentCulture { get; private set; }
-
-    /// <summary>
-    /// CultureInfo defined when the application started
-    /// </summary>
     private CultureInfo InitialCultureInfo { get; }
 
     public void Initialize()
@@ -83,25 +81,38 @@ public class LocalizationService : ILocalizationService
 
     private List<CultureDefinition> BuildAvailableCultures()
     {
-        CultureInfo cultureInfo;
-
         var cultureDefinitions = new HashSet<CultureDefinition>();
+        var culturesToLoad = new[] { "fr", "en" };
 
-        cultureInfo = CultureInfo.GetCultureInfo("fr");
-        cultureDefinitions.Add(new CultureDefinition
+        foreach (var cultureCode in culturesToLoad)
         {
-            Code = cultureInfo.Name,
-            Description = cultureInfo.NativeName,
-            CultureInfo = CultureInfo.GetCultureInfo(cultureInfo.Name)
-        });
+            try
+            {
+                var cultureInfo = CultureInfo.GetCultureInfo(cultureCode);
+                cultureDefinitions.Add(new CultureDefinition
+                {
+                    Code = cultureInfo.Name,
+                    Description = cultureInfo.NativeName,
+                    CultureInfo = cultureInfo
+                });
+            }
+            catch (CultureNotFoundException ex)
+            {
+                _logger.LogWarning("Culture \'{CultureCode}\' not found: {ExMessage}", cultureCode, ex.Message);
+            }
+        }
 
-        cultureInfo = CultureInfo.GetCultureInfo("en");
-        cultureDefinitions.Add(new CultureDefinition
+        if (!cultureDefinitions.Any())
         {
-            Code = cultureInfo.Name,
-            Description = cultureInfo.NativeName,
-            CultureInfo = CultureInfo.GetCultureInfo(cultureInfo.Name)
-        });
+            _logger.LogWarning("No valid cultures found. Adding invariant culture");
+            var invariantCulture = CultureInfo.InvariantCulture;
+            cultureDefinitions.Add(new CultureDefinition
+            {
+                Code = invariantCulture.Name,
+                Description = invariantCulture.NativeName,
+                CultureInfo = invariantCulture
+            });
+        }
 
         return cultureDefinitions.ToList();
     }
@@ -111,7 +122,7 @@ public class LocalizationService : ILocalizationService
         DoSetCurrentCulture(cultureDefinition);
         
         _applicationSettingsRepository.UpdateCurrentApplicationSettings(
-            settings => settings.CultureCode = CurrentCultureDefinition?.Code);
+            settings => settings.CultureCode = CurrentCultureDefinition.Code);
         
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Item"));
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Item[]"));
@@ -121,22 +132,7 @@ public class LocalizationService : ILocalizationService
     {
         _cultureDefinition.OnNext(cultureDefinition);
         
-        // CurrentCultureDefinition = cultureDefinition;
-        // var cultureInfo = CultureInfo.GetCultureInfo(cultureDefinition.Code);
-
-        // CurrentCulture = CultureInfo.GetCultureInfo(cultureDefinition.Code);
-
-        // _localizationService.CurrentCulture = cultureInfo;
-
-        // Example :
-        //  - DefaultCultureInfo fr-FR, cultureInfo fr => fr-FR
-        //  - DefaultCultureInfo fr, cultureInfo fr => fr
-        //  - DefaultCultureInfo en, cultureInfo fr => fr
-
-        // ReSharper disable once UnusedVariable : for debugging purposes
-        var cultureInfoBefore = CultureInfo.CurrentCulture;
-
-        if (Equals(InitialCultureInfo?.Parent, CurrentCultureDefinition.CultureInfo))
+        if (Equals(InitialCultureInfo.Parent, CurrentCultureDefinition.CultureInfo))
         {
             Thread.CurrentThread.CurrentCulture = InitialCultureInfo;
             Thread.CurrentThread.CurrentUICulture = InitialCultureInfo;
@@ -146,9 +142,6 @@ public class LocalizationService : ILocalizationService
             Thread.CurrentThread.CurrentCulture = CurrentCultureDefinition.CultureInfo;
             Thread.CurrentThread.CurrentUICulture = CurrentCultureDefinition.CultureInfo;
         }
-
-        // ReSharper disable once UnusedVariable : for debugging purposes
-        var cultureInfoAfter = CultureInfo.CurrentCulture;
     }
 
     public ReadOnlyCollection<CultureDefinition> GetAvailableCultures()
@@ -160,36 +153,16 @@ public class LocalizationService : ILocalizationService
     {
         get
         {
-            string? result = _resourceManager.GetString(key, CurrentCultureDefinition!.CultureInfo) ?? 
-                             _resourceManager.GetString(key.Replace("_", "."), CurrentCultureDefinition!.CultureInfo);
+            string? result = _resourceManager.GetString(key, CurrentCultureDefinition.CultureInfo) ?? 
+                             _resourceManager.GetString(key.Replace("_", "."), CurrentCultureDefinition.CultureInfo);
 
             return result!;
         }
     }
-
-    // public CultureInfo CurrentCulture
-    // {
-    //     get { return this._currentCulture!; }
-    //     set
-    //     {
-    //         if (!Equals(this._currentCulture, value))
-    //         {
-    //             this._currentCulture = value;
-    //
-    //             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Item"));
-    //             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Item[]"));
-    //         }
-    //     }
-    // }
-
-    /// <summary>
-    /// Renvoie le nom d'un mois dans la culture actuel
-    /// </summary>
-    /// <param name="monthNumber0to11">de 0 à 11, janvier à décembre</param>
-    /// <returns></returns>
-    public string GetMonthName(int monthNumber0to11)
+    
+    public string GetMonthName(int monthNumber0To11)
     {
-        string key = monthNumber0to11 switch
+        string key = monthNumber0To11 switch
         {
             0 => "General_January",
             1 => "General_February",
@@ -203,7 +176,7 @@ public class LocalizationService : ILocalizationService
             9 => "General_October",
             10 => "General_November",
             11 => "General_December",
-            _ => throw new ArgumentOutOfRangeException(nameof(monthNumber0to11))
+            _ => throw new ArgumentOutOfRangeException(nameof(monthNumber0To11))
         };
 
         return this[key];
