7b349127d6e1d4c48fac702482b3a203d250acca
Paul Fresquet <paul.fresquet@pow-software.com>
2025-01-22 07:45:56 +0100

    feat: improve arguments names

--- src/ByteSync.Client/Business/Arguments/RegularArguments.cs
+++ src/ByteSync.Client/Business/Arguments/RegularArguments.cs
@@ -2,29 +2,29 @@
 
 public static class RegularArguments
 {
-    internal const string WAIT_AFTER_RESTART = "-wait-after-restart";
+    internal const string WAIT_AFTER_RESTART = "--wait-after-restart";
         
-    internal const string LOG_DEBUG = "-log-debug";
+    internal const string LOG_DEBUG = "--log-debug";
         
-    internal const string SET_MACHINE_NAME = "-set-machine-name";
+    internal const string SET_MACHINE_NAME = "--set-machine-name=";
         
-    internal const string UPDATE = "-update";
+    internal const string UPDATE = "--update";
     
-    internal const string VERSION = "-version";
+    internal const string VERSION = "--version";
         
-    internal const string JOIN = "-join";
+    internal const string JOIN = "--join";
         
-    internal const string INVENTORY = "-inventory";
+    internal const string INVENTORY = "--inventory";
         
-    internal const string SYNCHRONIZE = "-synchronize";
+    internal const string SYNCHRONIZE = "--synchronize";
         
-    internal const string NO_GUI = "-no-gui";
+    internal const string NO_GUI = "--no-gui";
     
-    internal const string CF_API_URL_LOCAL_DEBUG = "-use-local-api";
+    internal const string CF_API_URL_LOCAL_DEBUG = "--use-local-api";
     
-    internal const string CF_API_URL_DEVELOPMENT = "-use-development-api";
+    internal const string CF_API_URL_DEVELOPMENT = "--use-development-api";
     
-    internal const string CF_API_URL_STAGING = "-use-staging-api";
+    internal const string CF_API_URL_STAGING = "--use-staging-api";
     
-    internal const string CF_API_URL_PRODUCTION = "-use-production-api";
+    internal const string CF_API_URL_PRODUCTION = "--use-production-api";
 }
