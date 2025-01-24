5bdad8e1c392caf2866b4811f40ff072f44be8d1
Paul Fresquet <paul.fresquet@pow-software.com>
2025-01-24 07:38:36 +0100

    feat: create view & viewmodels

---
+++ src/ByteSync.Client/ViewModels/Home/CreateCloudSessionViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ByteSync.ViewModels.Home;
+
+public class CreateCloudSessionViewModel : ViewModelBase
+{
+    
+}
---
+++ src/ByteSync.Client/Views/Home/JoinCloudSessionView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ByteSync.Views.Home;
+
+public partial class JoinCloudSessionView : UserControl
+{
+    public JoinCloudSessionView()
+    {
+        InitializeComponent();
+    }
+    
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
