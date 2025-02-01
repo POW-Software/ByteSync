3d257bf474d8d60fea6673e989bf34e45e20419f
Paul Fresquet <paul.fresquet@pow-software.com>
2025-02-01 09:13:51 +0100

    refactor: cleanup

--- src/ByteSync.Client/Services/Sessions/Connecting/QuitSessionService.cs
+++ src/ByteSync.Client/Services/Sessions/Connecting/QuitSessionService.cs
@@ -1,8 +1,6 @@
-﻿using ByteSync.Business.Navigations;
-using ByteSync.Business.Sessions;
+﻿using ByteSync.Business.Sessions;
 using ByteSync.Common.Business.Sessions.Cloud;
 using ByteSync.Interfaces.Controls.Communications.Http;
-using ByteSync.Interfaces.Controls.Navigations;
 using ByteSync.Interfaces.Controls.Sessions;
 using ByteSync.Interfaces.Services.Sessions.Connecting;
 
@@ -11,16 +9,14 @@ namespace ByteSync.Services.Sessions.Connecting;
 public class QuitSessionService : IQuitSessionService
 {
     private readonly ISessionService _sessionService;
-    private readonly INavigationService _navigationService;
     private readonly ICloudSessionApiClient _cloudSessionApiClient;
     private readonly ICloudSessionConnector _cloudSessionConnector;
     private readonly ILogger<QuitSessionService> _logger;
 
-    public QuitSessionService(ISessionService sessionService, INavigationService navigationService, ICloudSessionApiClient cloudSessionApiClient,
+    public QuitSessionService(ISessionService sessionService, ICloudSessionApiClient cloudSessionApiClient,
         ICloudSessionConnector cloudSessionConnector, ILogger<QuitSessionService> logger)
     {
         _sessionService = sessionService;
-        _navigationService = navigationService;
         _cloudSessionApiClient = cloudSessionApiClient;
         _cloudSessionConnector = cloudSessionConnector;
         _logger = logger;
@@ -58,9 +54,5 @@ public class QuitSessionService : IQuitSessionService
         {
             _logger.LogInformation("Quitted Local Session {CloudSession}", session.SessionId);
         }
-
-        //
-        // _navigationService.NavigateTo(NavigationPanel.Home);
-        // // _cloudSessionEventsHub.RaiseCloudSessionQuitted();
     }
 }
