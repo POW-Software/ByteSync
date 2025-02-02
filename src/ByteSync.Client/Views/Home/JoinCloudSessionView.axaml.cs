4cb839f32952ce72dab9f83ed671d3093adadd5e
Paul Fresquet <paul.fresquet@pow-software.com>
2025-02-02 15:28:52 +0100

    fix: restore "Enter" button shortcut on Join Session Form

--- src/ByteSync.Client/Views/Home/JoinCloudSessionView.axaml.cs
+++ src/ByteSync.Client/Views/Home/JoinCloudSessionView.axaml.cs
@@ -1,14 +1,47 @@
-﻿using Avalonia.Markup.Xaml;
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using ByteSync.ViewModels.Home;
+using ReactiveMarbles.ObservableEvents;
+using ReactiveUI;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace ByteSync.Views.Home;
 
 public class JoinCloudSessionView : ReactiveUserControl<JoinCloudSessionViewModel>
 {
+    public TextBox TextBoxSessionId => this.FindControl<TextBox>("TextBoxSessionId");
+        
+    public TextBox TextBoxSessionPassword => this.FindControl<TextBox>("TextBoxSessionPassword");
+        
+    public Button ButtonJoin => this.FindControl<Button>("ButtonJoin");
+    
     public JoinCloudSessionView()
     {
         InitializeComponent();
+        
+        this.WhenActivated(disposables =>
+        {
+            TextBoxSessionId.Events().KeyUp
+                .Where(k => k.Key == Key.Enter)
+                .Subscribe(e =>
+                {
+                    ButtonJoin.Focus();
+                    ViewModel?.JoinCommand.Execute().Subscribe();
+                })
+                .DisposeWith(disposables);
+                
+            TextBoxSessionPassword.Events().KeyUp
+                .Where(k => k.Key == Key.Enter)
+                .Subscribe(e =>
+                {
+                    ButtonJoin.Focus();
+                    ViewModel?.JoinCommand.Execute().Subscribe();
+                })
+                .DisposeWith(disposables);
+        });
     }
     
     private void InitializeComponent()
