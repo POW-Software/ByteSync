4648c8dd9bae984d650e5789db4c965e3315067e
Paul Fresquet <paul.fresquet@pow-software.com>
2025-02-08 12:02:06 +0100

    refactor: cleanup

--- src/ByteSync.Client/Business/SessionMembers/SessionMemberInfo.cs
+++ src/ByteSync.Client/Business/SessionMembers/SessionMemberInfo.cs
@@ -1,24 +1,19 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using ByteSync.Common.Business.EndPoints;
+﻿using ByteSync.Common.Business.EndPoints;
 using ByteSync.Common.Business.Sessions;
 using ByteSync.Common.Business.Sessions.Cloud;
-using ReactiveUI.Fody.Helpers;
 
 namespace ByteSync.Business.SessionMembers;
 
-public class SessionMemberInfo : INotifyPropertyChanged
+public class SessionMemberInfo
 {
     public ByteSyncEndpoint Endpoint { get; set; }
     
     public SessionMemberPrivateData PrivateData { get; set; }
     
     public string SessionId { get; set; }
-        
 
     public DateTimeOffset JoinedSessionOn { get; set; }
     
-    // [Reactive]
     public int PositionInList { get; set; }
     
     public DateTimeOffset? LastLocalInventoryGlobalStatusUpdate { get; set; }
@@ -28,8 +23,6 @@ public class SessionMemberInfo : INotifyPropertyChanged
     public string? LobbyId { get; set; }
     
     public string? ProfileClientId { get; set; }
-    
-    public string ClientId => Endpoint.ClientId;
     
     public string ClientInstanceId => Endpoint.ClientInstanceId;
     
@@ -58,20 +51,5 @@ public class SessionMemberInfo : INotifyPropertyChanged
     public override int GetHashCode()
     {
         return ClientInstanceId.GetHashCode();
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
     }
 }
