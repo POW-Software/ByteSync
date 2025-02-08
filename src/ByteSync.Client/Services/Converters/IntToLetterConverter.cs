ef6d7047b269efd06b0edf618dc14b5582f580f2
Paul Fresquet <paul.fresquet@pow-software.com>
2025-02-08 11:59:39 +0100

    feat: restore int to letter conversion

---
+++ src/ByteSync.Client/Services/Converters/IntToLetterConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace ByteSync.Services.Converters;
+
+public class IntToLetterConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        int iValue = System.Convert.ToInt32(value);
+        
+        return ((char) ('A' + iValue)).ToString();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string strValue && strValue.Length == 1)
+        {
+            char letter = strValue[0];
+            if (char.IsLetter(letter))
+            {
+                return char.ToUpper(letter) - 'A';
+            }
+        }
+
+        throw new ArgumentOutOfRangeException();
+    }
+}
