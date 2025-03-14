0f2ddb6c900e2749f487e5a56c2959b05bffeb2a
Paul Fresquet <paul.fresquet@pow-software.com>
2025-03-14 15:18:26 +0100

    refactor: comment translation

--- src/ByteSync.Client/Services/Communications/PublicKeysTruster.cs
+++ src/ByteSync.Client/Services/Communications/PublicKeysTruster.cs
@@ -52,7 +52,7 @@ public class PublicKeysTruster : IPublicKeysTruster
             }
         }
 
-        // Pour tous les membres non trustés, on demande un trust
+        // For all non-trusted members, a trust protocol is initiated
         if (nonFullyTrustedMembersIds.Count > 0)
         {
             var joinSessionResult = await DoTrustMembersPublicKeys(sessionId, nonFullyTrustedMembersIds, cancellationToken);
