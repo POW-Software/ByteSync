ee2cc2a72f80a72b45e1451438a6d7f5c54d3402
Paul Fresquet <paul.fresquet@pow-software.com>
2025-03-18 23:22:31 +0100

    refactor: cleanup

--- src/ByteSync.Client/Factories/ConnectionFactory.cs
+++ src/ByteSync.Client/Factories/ConnectionFactory.cs
@@ -83,56 +83,6 @@ public class ConnectionFactory : IConnectionFactory
 
         return isSuccess;
     }
-
-    // private async Task<HubConnection> StartConnection()
-    // {
-    //     var apiUrl = await _connectionConstantsService.GetApiUrl();
-    //     var url = UrlUtils.AppendSegment(apiUrl, "auth");
-    //     
-    //     var tokens = (await _authenticationTokensRepository.GetTokens())!;
-    //     
-    //     var connectionBuilder =
-    //
-    //         new HubConnectionBuilder()
-    //             .WithUrl(url,
-    //                 options =>
-    //                 {
-    //                     options.Headers.Add("Authorization", tokens.JwtToken);
-    //                     options.Transports = HttpTransportType.WebSockets;
-    //                 })
-    //         #if DEBUG
-    //             .ConfigureLogging(logging =>
-    //             {
-    //                 // This will set ALL logging to Debug level
-    //                 logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Debug);
-    //             })
-    //         #endif
-    //             .WithAutomaticReconnect(_connectionConstantsService.GetRetriesTimeSpans());
-    //
-    //     var connection = connectionBuilder.Build();
-    //
-    //     connection.Closed += _ =>
-    //     {
-    //         _logger.LogWarning("connection closed");
-    //         return Task.CompletedTask;
-    //     };
-    //     
-    //     connection.Reconnected += _ =>
-    //     {
-    //         _logger.LogWarning("connection reconnected");
-    //         return Task.CompletedTask;
-    //     };
-    //     
-    //     connection.Reconnecting += _ =>
-    //     {
-    //         _logger.LogWarning("connection reconnecting");
-    //         return Task.CompletedTask;
-    //     };
-    //
-    //     await connection.StartAsync();
-    //
-    //     return connection;
-    // }
     
     private async Task<InitialAuthenticationResponse?> GetInitialAuthenticationTokens()
     {
--- tests/ByteSync.Client.IntegrationTests/Services/Communications/TestConnectionService.cs
+++ tests/ByteSync.Client.IntegrationTests/Services/Communications/TestConnectionService.cs
@@ -7,14 +7,12 @@ using ByteSync.Client.IntegrationTests.TestHelpers;
 using ByteSync.Common.Business.Auth;
 using ByteSync.Common.Business.EndPoints;
 using ByteSync.Common.Business.Misc;
-using ByteSync.Exceptions;
 using ByteSync.Factories;
 using ByteSync.Interfaces.Controls.Applications;
 using ByteSync.Interfaces.Controls.Communications.Http;
 using ByteSync.Interfaces.Factories;
 using ByteSync.Interfaces.Services.Communications;
 using ByteSync.Services.Communications;
-using ByteSync.Services.Inventories;
 using ByteSync.TestsCommon;
 using FluentAssertions;
 using Microsoft.AspNetCore.Connections;
@@ -40,13 +38,6 @@ public class TestConnectionService : IntegrationTest
         RegisterType<ConnectionService>();
         BuildMoqContainer();
         
-        
-
-        var contextHelper = new TestContextGenerator(Container);
-
-        // _sessionId = contextHelper.GenerateSession();
-        // _currentEndPoint = contextHelper.GenerateCurrentEndpoint();
-        
         var mockEnvironmentService = Container.Resolve<Mock<IEnvironmentService>>();
         mockEnvironmentService.Setup(m => m.ClientId).Returns("test-client-id");
         mockEnvironmentService.Setup(m => m.ClientInstanceId).Returns("test-client-instance-id");
@@ -59,9 +50,6 @@ public class TestConnectionService : IntegrationTest
     [Test]
     public async Task StartConnectionAsync_SuccessfulConnection_ShouldUpdateConnectionStatus()
     {
-        // Arrange
-        // var mockHubConnection = new Mock<HubConnection>(MockBehavior.Loose); // new Mock<HubConnection>(MockBehavior.Strict, new Uri("http://test"), null);
-
         var mockHubConnection = new TestableHubConnection();
         
         var endPoint = new ByteSyncEndpoint 
@@ -69,16 +57,6 @@ public class TestConnectionService : IntegrationTest
             ClientInstanceId = "test-instance-id",
             ClientId = "test-id"
         };
-        
-        // var connectionResult = new ConnectionResult 
-        // { 
-        //     HubConnection = mockHubConnection.Object,
-        //     EndPoint = endPoint,
-        //     AuthenticateResponseStatus = InitialConnectionStatus.Success
-        // };
-        
-        // _mockConnectionFactory.Setup(f => f.BuildConnection())
-        //     .ReturnsAsync(connectionResult);
 
         var initialAuthenticationResponse = new InitialAuthenticationResponse
         {
@@ -110,166 +88,4 @@ public class TestConnectionService : IntegrationTest
         _connectionService.CurrentEndPoint.Should().Be(endPoint);
         _connectionService.ClientInstanceId.Should().Be(endPoint.ClientInstanceId);
     }
-
-    /*
-    [Test]
-    public async Task StartConnectionAsync_VersionNotAllowed_ShouldThrowException()
-    {
-        // Arrange
-        var connectionResult = new ConnectionResult 
-        { 
-            HubConnection = null,
-            EndPoint = null,
-            AuthenticateResponseStatus = InitialConnectionStatus.VersionNotAllowed
-        };
-        
-        _mockConnectionFactory.Setup(f => f.BuildConnection())
-            .ReturnsAsync(connectionResult);
-
-        // Act & Assert
-        await FluentActions.Invoking(async () => await _connectionService.StartConnectionAsync())
-            .Should()
-            .ThrowAsync<BuildConnectionException>()
-            .WithMessage("*VersionNotAllowed*");
-
-        _connectionService.CurrentConnectionStatus.Should().Be(ConnectionStatuses.NotConnected);
-    }
-
-    [Test]
-    public async Task StartConnectionAsync_ConnectionFailure_ShouldRetry()
-    {
-        // Arrange
-        int attemptCount = 0;
-        _mockConnectionFactory.Setup(f => f.BuildConnection())
-            .ReturnsAsync(() =>
-            {
-                attemptCount++;
-                if (attemptCount == 1)
-                {
-                    throw new Exception("Connection failure");
-                }
-                
-                var mockHubConnection = new Mock<HubConnection>(MockBehavior.Strict, new Uri("http://test"), null);
-                return new ConnectionResult
-                {
-                    HubConnection = mockHubConnection.Object,
-                    EndPoint = new ByteSyncEndpoint { ClientInstanceId = "test-instance-id" },
-                    AuthenticateResponseStatus = InitialConnectionStatus.Success
-                };
-            });
-
-        // Act
-        await _connectionService.StartConnectionAsync();
-
-        // Assert
-        attemptCount.Should().Be(2);
-        _connectionService.CurrentConnectionStatus.Should().Be(ConnectionStatuses.Connected);
-    }
-    */
-    
-    // public class TestableHubConnection : HubConnection
-    // {
-    //     public TestableHubConnection() : base(new MockConnectionContext(), new MockProtocol(), new MockEndPoint(), new MockServiceProvider(), new MockLoggerFactory())
-    //     {
-    //     }
-    //
-    //     // Implement essential methods needed by your tests
-    //     public override Task StartAsync(CancellationToken cancellationToken = default)
-    //     {
-    //         return Task.CompletedTask;
-    //     }
-    //
-    //     public override Task StopAsync(CancellationToken cancellationToken = default)
-    //     {
-    //         return Task.CompletedTask;
-    //     }
-    // }
-    //
-    // // Simple mock implementations for required dependencies
-    // public class MockConnectionContext { }
-    // public class MockProtocol { }
-    // public class MockEndPoint { }
-    // public class MockServiceProvider : IServiceProvider
-    // {
-    //     public object GetService(Type serviceType) => null;
-    // }
-    // public class MockLoggerFactory { }
-    
-    // Test implementation of HubConnection that doesn't need constructor parameters
-public class TestableHubConnection : HubConnection
-{
-    private static readonly TestConnectionContext ConnectionContext = new();
-    private static readonly TestHubProtocol Protocol = new();
-    private static readonly TestEndPoint EndPoint = new();
-    private static readonly TestServiceProvider ServiceProvider = new();
-    private static readonly TestLoggerFactory LoggerFactory = new();
-
-    public TestableHubConnection()
-        : base(new TestConnectionFactory(), Protocol, EndPoint, ServiceProvider, LoggerFactory)
-    {
-    }
-
-    // Mock implementations to provide constructor parameters
-    private class TestConnectionFactory : Microsoft.AspNetCore.Connections.IConnectionFactory
-    {
-        public ValueTask<ConnectionContext> ConnectAsync(EndPoint endPoint, CancellationToken cancellationToken = default)
-        {
-            return ValueTask.FromResult<ConnectionContext>(ConnectionContext);
-        }
-    }
-
-    private class TestConnectionContext : ConnectionContext
-    {
-        public override string ConnectionId { get; set; }
-        public override IFeatureCollection Features { get; } = new FeatureCollection();
-        public override IDictionary<object, object?> Items { get; set; }
-        public override IDuplexPipe Transport { get; set; } = null!;
-    }
-
-    private class TestHubProtocol : IHubProtocol
-    {
-        public string Name => "test-protocol";
-        public int Version => 1;
-        public TransferFormat TransferFormat => TransferFormat.Binary;
-
-        public bool IsVersionSupported(int version) => true;
-        public ReadOnlyMemory<byte> GetMessageBytes(HubMessage message) => new byte[0];
-        public bool TryParseMessage(ref ReadOnlySequence<byte> input, IInvocationBinder binder, out HubMessage message)
-        {
-            message = null!;
-            return false;
-        }
-        public void WriteMessage(HubMessage message, IBufferWriter<byte> output) { }
-    }
-
-    private class TestEndPoint : EndPoint { }
-
-    private class TestServiceProvider : IServiceProvider
-    {
-        public object? GetService(Type serviceType) => null;
-    }
-
-    private class TestLoggerFactory : ILoggerFactory
-    {
-        public void Dispose() { }
-        public ILogger CreateLogger(string categoryName) => new TestLogger();
-        public void AddProvider(ILoggerProvider provider) { }
-    }
-
-    private class TestLogger : ILogger
-    {
-        public IDisposable BeginScope<TState>(TState state) where TState : notnull => new TestDisposable();
-        public bool IsEnabled(LogLevel logLevel) => false;
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) { }
-    }
-
-    private class TestDisposable : IDisposable
-    {
-        public void Dispose() { }
-    }
-
-    // Override the abstract/virtual methods we need
-    public override Task StartAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
-    public override Task StopAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
-}
 }
