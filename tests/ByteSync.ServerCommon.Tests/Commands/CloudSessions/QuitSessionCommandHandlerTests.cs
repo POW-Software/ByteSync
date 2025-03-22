98b6a9c16b082e06586df7384da0b77b7e7e2555
Paul Fresquet <61119222+paul-fresquet@users.noreply.github.com>
2025-03-22 07:42:24 +0100

    Improve QuitSessionCommandHandler (#60)

--- tests/ByteSync.ServerCommon.Tests/Commands/CloudSessions/QuitSessionCommandHandlerTests.cs
+++ tests/ByteSync.ServerCommon.Tests/Commands/CloudSessions/QuitSessionCommandHandlerTests.cs
@@ -1,4 +1,6 @@
-﻿using ByteSync.Common.Business.Sessions.Cloud;
+﻿using ByteSync.Common.Business.Inventories;
+using ByteSync.Common.Business.Sessions;
+using ByteSync.Common.Business.Sessions.Cloud;
 using ByteSync.Common.Interfaces.Hub;
 using ByteSync.ServerCommon.Business.Auth;
 using ByteSync.ServerCommon.Business.Sessions;
@@ -146,7 +148,7 @@ public class QuitSessionCommandHandlerTests
     }
 
     [Test]
-    public async Task QuitSession_SessionMemberNotExists_()
+    public async Task QuitSession_SessionMemberNotExists_DoesNotUpdateOrNotify()
     {
         // Arrange
         var sessionId = "testSession";
@@ -225,4 +227,79 @@ public class QuitSessionCommandHandlerTests
         A.CallTo(() => _mockSessionMemberMapper.Convert(A<SessionMemberData>.Ignored))
             .MustNotHaveHappened();
     }
+    
+    [Test]
+public async Task QuitSession_WithPathItems_NotifiesPathItemRemoved()
+{
+    // Arrange
+    var sessionId = "testSession";
+    var client = new Client { ClientInstanceId = "clientInstance1" };
+    
+    var sessionMember = new SessionMemberData { ClientInstanceId = "clientInstance1" };
+    var cloudSessionData = new CloudSessionData();
+    cloudSessionData.SessionMembers.Add(sessionMember);
+    cloudSessionData.IsSessionActivated = true;
+
+    // Create inventory data with path items
+    var inventoryData = new InventoryData(sessionId);
+    var inventoryMember = new InventoryMemberData { ClientInstanceId = "clientInstance1" };
+    var pathItem1 = new EncryptedPathItem { Code = "path1", Data = new byte[] { 1, 2, 3 }, IV = new byte[] { 4, 5, 6 } };
+    var pathItem2 = new EncryptedPathItem { Code = "path2", Data = new byte[] { 7, 8, 9 }, IV = new byte[] { 10, 11, 12 } };
+    inventoryMember.SharedPathItems.Add(pathItem1);
+    inventoryMember.SharedPathItems.Add(pathItem2);
+    inventoryData.InventoryMembers.Add(inventoryMember);
+    
+    var synchronizationEntity = new SynchronizationEntity
+    {
+        SessionId = sessionId,
+        EndedOn = DateTimeOffset.Now
+    };
+
+    bool funcResult = false;
+    bool isTransaction = false;
+    A.CallTo(() => _mockCloudSessionsRepository.Update(A<string>.Ignored, A<Func<CloudSessionData, bool>>.Ignored, 
+            A<ITransaction>.Ignored, A<IRedLock>.Ignored))
+        .Invokes((string _, Func<CloudSessionData, bool> func, ITransaction? transaction, IRedLock? _) =>
+        {
+            funcResult = func(cloudSessionData);
+            isTransaction = transaction != null;
+        })
+        .ReturnsLazily(() => UpdateResultBuilder.BuildUpdateResult(funcResult, cloudSessionData, isTransaction));
+    
+    A.CallTo(() => _mockInventoryRepository.UpdateIfExists(A<string>.Ignored, A<Func<InventoryData, bool>>.Ignored, _mockTransaction, null))
+        .Invokes((string _, Func<InventoryData, bool> func, ITransaction? transaction, IRedLock? _) =>
+        {
+            funcResult = func(inventoryData);
+            isTransaction = transaction != null;
+        })
+        .ReturnsLazily(() => UpdateResultBuilder.BuildUpdateResult(funcResult, inventoryData, isTransaction));
+    
+    A.CallTo(() => _mockSynchronizationRepository.UpdateIfExists(sessionId, A<Func<SynchronizationEntity, bool>>.Ignored, _mockTransaction, null))
+        .Invokes((string _, Func<SynchronizationEntity, bool> func, ITransaction? transaction, IRedLock? _) =>
+        {
+            funcResult = func(synchronizationEntity);
+            isTransaction = transaction != null;
+        })
+        .ReturnsLazily(() => UpdateResultBuilder.BuildUpdateResult(funcResult, synchronizationEntity, isTransaction));
+    
+    A.CallTo(() => _mockTransaction.ExecuteAsync(CommandFlags.None)).Returns(true);
+    
+    var mockGroup = A.Fake<IHubByteSyncPush>();
+    A.CallTo(() => _mockInvokeClientsService.SessionGroup(sessionId)).Returns(mockGroup);
+    
+    var sessionMemberInfo = new SessionMemberInfoDTO();
+    A.CallTo(() => _mockSessionMemberMapper.Convert(sessionMember))
+        .Returns(Task.FromResult(sessionMemberInfo));
+    
+    // Act
+    var request = new QuitSessionRequest(sessionId, client);
+    await _quitSessionCommandHandler.Handle(request, CancellationToken.None);
+    
+    // Assert
+    A.CallTo(() => mockGroup.PathItemRemoved(A<PathItemDTO>.That.Matches(dto => 
+        dto.SessionId == sessionId && 
+        dto.ClientInstanceId == client.ClientInstanceId && 
+        (dto.EncryptedPathItem.Code == "path1" || dto.EncryptedPathItem.Code == "path2"))))
+        .MustHaveHappened(2, Times.Exactly);
+}
 }
