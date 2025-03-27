637d31a95f34d6b3664dbadcffa23aa9779642f3
Paul Fresquet <paul.fresquet@pow-software.com>
2025-03-27 07:43:46 +0100

    refactor: to file-scoped namespace

--- src/ByteSync.Client/Models/Comparisons/Result/ContentIdentity.cs
+++ src/ByteSync.Client/Models/Comparisons/Result/ContentIdentity.cs
@@ -2,205 +2,204 @@
 using ByteSync.Models.FileSystems;
 using ByteSync.Models.Inventories;
 
-namespace ByteSync.Models.Comparisons.Result
+namespace ByteSync.Models.Comparisons.Result;
+
+public class ContentIdentity
 {
-    public class ContentIdentity
+    public ContentIdentity(ContentIdentityCore? contentIdentityCore)
     {
-        public ContentIdentity(ContentIdentityCore? contentIdentityCore)
-        {
-            Core = contentIdentityCore;
+        Core = contentIdentityCore;
 
-            FileSystemDescriptions = new HashSet<FileSystemDescription>();
-            InventoryPartsByCreationTimes = new Dictionary<DateTime, HashSet<InventoryPart>>();
-            InventoryPartsByLastWriteTimes = new Dictionary<DateTime, HashSet<InventoryPart>>();
-            FileSystemDescriptionsByInventoryParts = new Dictionary<InventoryPart, HashSet<FileSystemDescription>>();
-        }
+        FileSystemDescriptions = new HashSet<FileSystemDescription>();
+        InventoryPartsByCreationTimes = new Dictionary<DateTime, HashSet<InventoryPart>>();
+        InventoryPartsByLastWriteTimes = new Dictionary<DateTime, HashSet<InventoryPart>>();
+        FileSystemDescriptionsByInventoryParts = new Dictionary<InventoryPart, HashSet<FileSystemDescription>>();
+    }
 
-        public ContentIdentityCore? Core { get; }
+    public ContentIdentityCore? Core { get; }
 
-        public HashSet<FileSystemDescription> FileSystemDescriptions { get; }
+    public HashSet<FileSystemDescription> FileSystemDescriptions { get; }
         
-        private Dictionary<DateTime, HashSet<InventoryPart>> InventoryPartsByCreationTimes { get; }
+    private Dictionary<DateTime, HashSet<InventoryPart>> InventoryPartsByCreationTimes { get; }
         
-        public Dictionary<DateTime, HashSet<InventoryPart>> InventoryPartsByLastWriteTimes { get; }
+    public Dictionary<DateTime, HashSet<InventoryPart>> InventoryPartsByLastWriteTimes { get; }
         
-        private Dictionary<InventoryPart, HashSet<FileSystemDescription>> FileSystemDescriptionsByInventoryParts { get; }
+    private Dictionary<InventoryPart, HashSet<FileSystemDescription>> FileSystemDescriptionsByInventoryParts { get; }
 
-        public bool HasAnalysisError
+    public bool HasAnalysisError
+    {
+        get
         {
-            get
-            {
-                return FileSystemDescriptions.Any(fsd => fsd is FileDescription { HasAnalysisError: true });
-            }
+            return FileSystemDescriptions.Any(fsd => fsd is FileDescription { HasAnalysisError: true });
+        }
+    }
+
+    public bool HasManyFileSystemDescriptionOnAnInventoryPart
+    {
+        get
+        {
+            return FileSystemDescriptionsByInventoryParts.Any(p => p.Value.Count > 1);
+        }
+    }
+
+    protected bool Equals(ContentIdentity other)
+    {
+        return Equals(Core, other.Core);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((ContentIdentity) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return (Core != null ? Core.GetHashCode() : 0);
+    }
+
+    public override string ToString()
+    {
+    #if DEBUG
+        string toString = $"ContentIdentity {Core?.SignatureHash} {Core?.Size}";
+
+        foreach (var inventoryPartsByDate in InventoryPartsByLastWriteTimes)
+        {
+            toString += $" - '{inventoryPartsByDate.Key:G}' {inventoryPartsByDate.Value.Select(ip => ip.RootName).ToList().JoinToString(", ")}"; 
         }
 
-        public bool HasManyFileSystemDescriptionOnAnInventoryPart
-        {
-            get
-            {
-                return FileSystemDescriptionsByInventoryParts.Any(p => p.Value.Count > 1);
-            }
-        }
-
-        protected bool Equals(ContentIdentity other)
-        {
-            return Equals(Core, other.Core);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((ContentIdentity) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return (Core != null ? Core.GetHashCode() : 0);
-        }
-
-        public override string ToString()
-        {
-#if DEBUG
-            string toString = $"ContentIdentity {Core?.SignatureHash} {Core?.Size}";
-
-            foreach (var inventoryPartsByDate in InventoryPartsByLastWriteTimes)
-            {
-                toString += $" - '{inventoryPartsByDate.Key:G}' {inventoryPartsByDate.Value.Select(ip => ip.RootName).ToList().JoinToString(", ")}"; 
-            }
-
-            return toString;
-#endif
+        return toString;
+    #endif
 
 #pragma warning disable 162
-            return base.ToString();
+        return base.ToString();
 #pragma warning restore 162
-        }
+    }
 
-        public bool IsPresentIn(InventoryPart inventoryPart)
+    public bool IsPresentIn(InventoryPart inventoryPart)
+    {
+        foreach (var pair in FileSystemDescriptionsByInventoryParts)
         {
-            foreach (var pair in FileSystemDescriptionsByInventoryParts)
+            if (pair.Key.Equals(inventoryPart))
             {
-                if (pair.Key.Equals(inventoryPart))
-                {
-                    return true;
-                }
+                return true;
             }
-
-            return false;
         }
+
+        return false;
+    }
         
-        public bool IsPresentIn(Inventory inventory)
+    public bool IsPresentIn(Inventory inventory)
+    {
+        foreach (var pair in FileSystemDescriptionsByInventoryParts)
         {
-            foreach (var pair in FileSystemDescriptionsByInventoryParts)
+            if (pair.Key.Inventory.Equals(inventory))
             {
-                if (pair.Key.Inventory.Equals(inventory))
-                {
-                    return true;
-                }
+                return true;
             }
-
-            return false;
         }
+
+        return false;
+    }
         
-        public HashSet<Inventory> GetInventories()
+    public HashSet<Inventory> GetInventories()
+    {
+        HashSet<Inventory> inventories = new HashSet<Inventory>();
+
+        foreach (var pair in FileSystemDescriptionsByInventoryParts)
         {
-            HashSet<Inventory> inventories = new HashSet<Inventory>();
-
-            foreach (var pair in FileSystemDescriptionsByInventoryParts)
-            {
-                inventories.Add(pair.Key.Inventory);
-            }
-
-            return inventories;
+            inventories.Add(pair.Key.Inventory);
         }
+
+        return inventories;
+    }
         
-        public HashSet<InventoryPart> GetInventoryParts()
-        {
-            HashSet<InventoryPart> result = new HashSet<InventoryPart>();
+    public HashSet<InventoryPart> GetInventoryParts()
+    {
+        HashSet<InventoryPart> result = new HashSet<InventoryPart>();
             
-            foreach (var pair in FileSystemDescriptionsByInventoryParts)
-            {
-                result.Add(pair.Key);
-            }
+        foreach (var pair in FileSystemDescriptionsByInventoryParts)
+        {
+            result.Add(pair.Key);
+        }
 
+        return result;
+    }
+
+    public DateTime? GetLastWriteTimeUtc(InventoryPart inventoryPart)
+    {
+        foreach (var pair in InventoryPartsByLastWriteTimes)
+        {
+            if (pair.Value.Contains(inventoryPart))
+            {
+                return pair.Key;
+            }
+        }
+
+        return null;
+    }
+        
+    public DateTime? GetCreationTimeUtc(InventoryPart inventoryPart)
+    {
+        foreach (var pair in InventoryPartsByCreationTimes)
+        {
+            if (pair.Value.Contains(inventoryPart))
+            {
+                return pair.Key;
+            }
+        }
+
+        return null;
+    }
+
+    public void Add(FileSystemDescription fileSystemDescription)
+    {
+        FileSystemDescriptions.Add(fileSystemDescription);
+
+        if (!FileSystemDescriptionsByInventoryParts.ContainsKey(fileSystemDescription.InventoryPart))
+        {
+            FileSystemDescriptionsByInventoryParts.Add(fileSystemDescription.InventoryPart, new HashSet<FileSystemDescription>());
+        }
+        FileSystemDescriptionsByInventoryParts[fileSystemDescription.InventoryPart].Add(fileSystemDescription);
+
+        if (fileSystemDescription is FileDescription fileDescription)
+        {
+            AddInventoryPartByCreationTime(fileSystemDescription.InventoryPart, fileDescription.CreationTimeUtc);
+            AddInventoryPartByLastWriteTime(fileSystemDescription.InventoryPart, fileDescription.LastWriteTimeUtc);
+        }
+    }
+        
+    private void AddInventoryPartByCreationTime(InventoryPart inventoryPart, DateTime creationTimeUtc)
+    {
+        if (!InventoryPartsByCreationTimes.ContainsKey(creationTimeUtc))
+        {
+            InventoryPartsByCreationTimes.Add(creationTimeUtc, new HashSet<InventoryPart>());
+        }
+
+        InventoryPartsByCreationTimes[creationTimeUtc].Add(inventoryPart);
+    }
+        
+    private void AddInventoryPartByLastWriteTime(InventoryPart inventoryPart, DateTime lastWriteTimeUtc)
+    {
+        if (!InventoryPartsByLastWriteTimes.ContainsKey(lastWriteTimeUtc))
+        {
+            InventoryPartsByLastWriteTimes.Add(lastWriteTimeUtc, new HashSet<InventoryPart>());
+        }
+
+        InventoryPartsByLastWriteTimes[lastWriteTimeUtc].Add(inventoryPart);
+    }
+
+    public HashSet<FileSystemDescription> GetFileSystemDescriptions(InventoryPart inventoryPart)
+    {
+        if (FileSystemDescriptionsByInventoryParts.TryGetValue(inventoryPart, out var result))
+        {
             return result;
         }
-
-        public DateTime? GetLastWriteTimeUtc(InventoryPart inventoryPart)
+        else
         {
-            foreach (var pair in InventoryPartsByLastWriteTimes)
-            {
-                if (pair.Value.Contains(inventoryPart))
-                {
-                    return pair.Key;
-                }
-            }
-
-            return null;
-        }
-        
-        public DateTime? GetCreationTimeUtc(InventoryPart inventoryPart)
-        {
-            foreach (var pair in InventoryPartsByCreationTimes)
-            {
-                if (pair.Value.Contains(inventoryPart))
-                {
-                    return pair.Key;
-                }
-            }
-
-            return null;
-        }
-
-        public void Add(FileSystemDescription fileSystemDescription)
-        {
-            FileSystemDescriptions.Add(fileSystemDescription);
-
-            if (!FileSystemDescriptionsByInventoryParts.ContainsKey(fileSystemDescription.InventoryPart))
-            {
-                FileSystemDescriptionsByInventoryParts.Add(fileSystemDescription.InventoryPart, new HashSet<FileSystemDescription>());
-            }
-            FileSystemDescriptionsByInventoryParts[fileSystemDescription.InventoryPart].Add(fileSystemDescription);
-
-            if (fileSystemDescription is FileDescription fileDescription)
-            {
-                AddInventoryPartByCreationTime(fileSystemDescription.InventoryPart, fileDescription.CreationTimeUtc);
-                AddInventoryPartByLastWriteTime(fileSystemDescription.InventoryPart, fileDescription.LastWriteTimeUtc);
-            }
-        }
-        
-        private void AddInventoryPartByCreationTime(InventoryPart inventoryPart, DateTime creationTimeUtc)
-        {
-            if (!InventoryPartsByCreationTimes.ContainsKey(creationTimeUtc))
-            {
-                InventoryPartsByCreationTimes.Add(creationTimeUtc, new HashSet<InventoryPart>());
-            }
-
-            InventoryPartsByCreationTimes[creationTimeUtc].Add(inventoryPart);
-        }
-        
-        private void AddInventoryPartByLastWriteTime(InventoryPart inventoryPart, DateTime lastWriteTimeUtc)
-        {
-            if (!InventoryPartsByLastWriteTimes.ContainsKey(lastWriteTimeUtc))
-            {
-                InventoryPartsByLastWriteTimes.Add(lastWriteTimeUtc, new HashSet<InventoryPart>());
-            }
-
-            InventoryPartsByLastWriteTimes[lastWriteTimeUtc].Add(inventoryPart);
-        }
-
-        public HashSet<FileSystemDescription> GetFileSystemDescriptions(InventoryPart inventoryPart)
-        {
-            if (FileSystemDescriptionsByInventoryParts.TryGetValue(inventoryPart, out var result))
-            {
-                return result;
-            }
-            else
-            {
-                return new HashSet<FileSystemDescription>();
-            }
+            return new HashSet<FileSystemDescription>();
         }
     }
 }
--- src/ByteSync.Client/Models/Comparisons/Result/Status.cs
+++ src/ByteSync.Client/Models/Comparisons/Result/Status.cs
@@ -2,49 +2,48 @@
 using ByteSync.Common.Business.Inventories;
 using ByteSync.Models.Inventories;
 
-namespace ByteSync.Models.Comparisons.Result
+namespace ByteSync.Models.Comparisons.Result;
+
+public class Status
 {
-    public class Status
+    public Status(PathIdentity pathIdentity)
     {
-        public Status(PathIdentity pathIdentity)
-        {
-            PathIdentity = pathIdentity;
+        PathIdentity = pathIdentity;
             
-            FingerPrintGroups = new Dictionary<ContentIdentityCore, HashSet<InventoryPart>>();
-            LastWriteTimeGroups = new Dictionary<DateTime, HashSet<InventoryPart>>();
+        FingerPrintGroups = new Dictionary<ContentIdentityCore, HashSet<InventoryPart>>();
+        LastWriteTimeGroups = new Dictionary<DateTime, HashSet<InventoryPart>>();
 
-            MissingInventories = new HashSet<Inventory>();
-            MissingInventoryParts = new HashSet<InventoryPart>();
-        }
+        MissingInventories = new HashSet<Inventory>();
+        MissingInventoryParts = new HashSet<InventoryPart>();
+    }
         
-        public PathIdentity PathIdentity { get; }
+    public PathIdentity PathIdentity { get; }
 
-        public Dictionary<ContentIdentityCore, HashSet<InventoryPart>> FingerPrintGroups { get; private set; }
+    public Dictionary<ContentIdentityCore, HashSet<InventoryPart>> FingerPrintGroups { get; private set; }
 
-        public Dictionary<DateTime, HashSet<InventoryPart>> LastWriteTimeGroups { get; private set; }
+    public Dictionary<DateTime, HashSet<InventoryPart>> LastWriteTimeGroups { get; private set; }
 
-        public HashSet<Inventory> MissingInventories { get; set; }
+    public HashSet<Inventory> MissingInventories { get; set; }
         
-        public HashSet<InventoryPart> MissingInventoryParts { get; set; }
+    public HashSet<InventoryPart> MissingInventoryParts { get; set; }
         
-        public bool IsSuccessStatus { get; set; }
-        
-        public bool IsErrorStatus { get; set; }
-
-        public bool IsOK
+    public bool IsSuccessStatus { get; set; }
+    
+    public bool IsErrorStatus { get; set; }
+    
+    public bool IsOK
+    {
+        get
         {
-            get
+            if (PathIdentity.FileSystemType == FileSystemTypes.File)
             {
-                if (PathIdentity.FileSystemType == FileSystemTypes.File)
-                {
-                    return FingerPrintGroups.Count == 1 && LastWriteTimeGroups.Count == 1 && MissingInventories.Count == 0
-                           && MissingInventoryParts.Count(ip => ip.InventoryPartType == FileSystemTypes.Directory) == 0;
-                }
-                else
-                {
-                    return MissingInventories.Count == 0
-                           && MissingInventoryParts.Count(ip => ip.InventoryPartType == FileSystemTypes.Directory) == 0;
-                }
+                return FingerPrintGroups.Count == 1 && LastWriteTimeGroups.Count == 1 && MissingInventories.Count == 0
+                       && MissingInventoryParts.Count(ip => ip.InventoryPartType == FileSystemTypes.Directory) == 0;
+            }
+            else
+            {
+                return MissingInventories.Count == 0
+                       && MissingInventoryParts.Count(ip => ip.InventoryPartType == FileSystemTypes.Directory) == 0;
             }
         }
     }
