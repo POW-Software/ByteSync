6ea2713746e70bf6940239b8a97eaea103d12fd3
Paul Fresquet <paul.fresquet@pow-software.com>
2025-04-01 20:29:36 +0200

    refactor: delete commented files refactor: cleanup

--- src/ByteSync.Client/Interfaces/ILocalApplicationDataManager.cs
+++ src/ByteSync.Client/Interfaces/ILocalApplicationDataManager.cs
@@ -2,12 +2,6 @@
 
 public interface ILocalApplicationDataManager
 {
-    // string ClientInstanceId { get; }
-    //
-    // ByteSyncEndpoint? CurrentEndPoint { get; set; }
-
-    // Task ExploreApplicationDataPath();
-        
     string ApplicationDataPath { get; }
         
     string? LogFilePath { get; }
