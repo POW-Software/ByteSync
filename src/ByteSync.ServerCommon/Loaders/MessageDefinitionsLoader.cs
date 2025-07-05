59a98c9a8957e63f003d7aade324432b627e357a
Paul Fresquet <61119222+paul-fresquet@users.noreply.github.com>
2025-07-05 14:09:13 +0000

    Add message definitions loader and timer

---
+++ src/ByteSync.ServerCommon/Loaders/MessageDefinitionsLoader.cs
@@ -0,0 +1,47 @@
+using ByteSync.Common.Controls.Json;
+using ByteSync.ServerCommon.Business.Messages;
+using ByteSync.ServerCommon.Business.Settings;
+using ByteSync.ServerCommon.Interfaces.Loaders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly;
+
+namespace ByteSync.ServerCommon.Loaders;
+
+public class MessageDefinitionsLoader : IMessageDefinitionsLoader
+{
+    private readonly AppSettings _appSettings;
+    private readonly ILogger<MessageDefinitionsLoader> _logger;
+
+    public MessageDefinitionsLoader(IOptions<AppSettings> appSettings, ILogger<MessageDefinitionsLoader> logger)
+    {
+        _appSettings = appSettings.Value;
+        _logger = logger;
+    }
+
+    public async Task<List<MessageDefinition>> Load()
+    {
+        List<MessageDefinition>? definitions = null;
+
+        var policy = Policy
+            .Handle<Exception>()
+            .WaitAndRetry(3, retryAttempt => TimeSpan.FromSeconds(3 * (retryAttempt + 1)));
+
+        await policy.Execute(async () =>
+        {
+            string contents;
+            using var httpClient = new HttpClient();
+            _logger.LogInformation("Loading messages from {url}", _appSettings.MessagesDefinitionsUrl);
+            contents = await httpClient.GetStringAsync(_appSettings.MessagesDefinitionsUrl);
+
+            definitions = JsonHelper.Deserialize<List<MessageDefinition>>(contents);
+        });
+
+        if (definitions == null)
+        {
+            throw new Exception("Failed to load messages");
+        }
+
+        return definitions!;
+    }
+}
