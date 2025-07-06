d03fff06e932035ec0db379d6786c2f8c492050f
Paul Fresquet <paul.fresquet@pow-software.com>
2025-07-06 18:58:51 +0200

    refactor: cleanup

--- tests/ByteSync.ServerCommon.Tests/Loaders/ClientSoftwareVersionSettingsLoaderTests.cs
+++ tests/ByteSync.ServerCommon.Tests/Loaders/ClientSoftwareVersionSettingsLoaderTests.cs
@@ -158,25 +158,3 @@ public class ClientSoftwareVersionSettingsLoaderTests
             .Should().ThrowAsync<Exception>();
     }
 }
-
-// public class MockHttpMessageHandler : HttpMessageHandler
-// {
-//     private readonly string _responseContent;
-//     private readonly HttpStatusCode _statusCode;
-//
-//     public MockHttpMessageHandler(string responseContent, HttpStatusCode statusCode = HttpStatusCode.OK)
-//     {
-//         _responseContent = responseContent;
-//         _statusCode = statusCode;
-//     }
-//
-//     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-//     {
-//         var response = new HttpResponseMessage(_statusCode)
-//         {
-//             Content = new StringContent(_responseContent, Encoding.UTF8, "application/json")
-//         };
-//
-//         return Task.FromResult(response);
-//     }
-// } 
