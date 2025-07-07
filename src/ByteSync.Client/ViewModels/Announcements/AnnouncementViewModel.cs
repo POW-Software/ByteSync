df2cdf62e77c3a79cb1ef467e0f98066ce8ba775
Paul Fresquet <paul.fresquet@pow-software.com>
2025-07-07 20:53:09 +0200

    fix: add missing using

--- src/ByteSync.Client/ViewModels/Announcements/AnnouncementViewModel.cs
+++ src/ByteSync.Client/ViewModels/Announcements/AnnouncementViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Disposables;
 using ByteSync.Common.Business.Announcements;
 using ByteSync.Interfaces;
 using ByteSync.Interfaces.Repositories;
