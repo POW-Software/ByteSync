2ada8a68aad251bce5d0ac6f2ad3e736c33001dc
Paul Fresquet <paul.fresquet@pow-software.com>
2025-07-29 19:14:10 +0200

    refactor: improve FileUploaderFactory

--- src/ByteSync.Client/Factories/FileUploaderFactory.cs
+++ src/ByteSync.Client/Factories/FileUploaderFactory.cs
@@ -23,44 +23,15 @@ public class FileUploaderFactory : IFileUploaderFactory
 
     public IFileUploader Build(string fullName, SharedFileDefinition sharedFileDefinition)
     {
-        // Create the slicer encrypter
-        var slicerEncrypter = _context.Resolve<ISlicerEncrypter>();
-        
-        // Create coordination components
-        var fileUploadCoordinator = new FileUploadCoordinator(_context.Resolve<ILogger<FileUploadCoordinator>>());
-        var semaphoreSlim = new SemaphoreSlim(1, 1);
-        
-        // Create file slicer
-        var fileSlicer = new FileSlicer(slicerEncrypter, fileUploadCoordinator.AvailableSlices, 
-            semaphoreSlim, fileUploadCoordinator.ExceptionOccurred, _context.Resolve<ILogger<FileSlicer>>());
-        
-        // Create file upload worker
-        var policyFactory = _context.Resolve<IPolicyFactory>();
-        var fileTransferApiClient = _context.Resolve<IFileTransferApiClient>();
-        var fileUploadWorker = new FileUploadWorker(policyFactory, fileTransferApiClient, sharedFileDefinition,
-            semaphoreSlim, fileUploadCoordinator.ExceptionOccurred, 
-            fileUploadCoordinator.UploadingIsFinished, _context.Resolve<ILogger<FileUploadWorker>>());
-        
-        // Create file part upload asserter
-        var sessionService = _context.Resolve<ISessionService>();
-        var filePartUploadAsserter = new FilePartUploadAsserter(fileTransferApiClient, sessionService);
-        
-        var fileUploader = _context.Resolve<IFileUploader>(
-            new TypedParameter(typeof(string), fullName),
-            new TypedParameter(typeof(MemoryStream), null),
-            new TypedParameter(typeof(SharedFileDefinition), sharedFileDefinition),
-            new TypedParameter(typeof(IFileUploadCoordinator), fileUploadCoordinator),
-            new TypedParameter(typeof(IFileSlicer), fileSlicer),
-            new TypedParameter(typeof(IFileUploadWorker), fileUploadWorker),
-            new TypedParameter(typeof(IFilePartUploadAsserter), filePartUploadAsserter),
-            new TypedParameter(typeof(ISlicerEncrypter), slicerEncrypter),
-            new TypedParameter(typeof(SemaphoreSlim), semaphoreSlim)
-        );
-        
-        return fileUploader;
+        return DoBuild(fullName, null, sharedFileDefinition);
     }
 
     public IFileUploader Build(MemoryStream memoryStream, SharedFileDefinition sharedFileDefinition)
+    {
+        return DoBuild(null, memoryStream, sharedFileDefinition);
+    }
+    
+    private IFileUploader DoBuild(string? fullName, MemoryStream? memoryStream, SharedFileDefinition sharedFileDefinition)
     {
         // Create the slicer encrypter
         var slicerEncrypter = _context.Resolve<ISlicerEncrypter>();
@@ -85,7 +56,7 @@ public class FileUploaderFactory : IFileUploaderFactory
         var filePartUploadAsserter = new FilePartUploadAsserter(fileTransferApiClient, sessionService);
         
         var fileUploader = _context.Resolve<IFileUploader>(
-            new TypedParameter(typeof(string), null),
+            new TypedParameter(typeof(string), fullName),
             new TypedParameter(typeof(MemoryStream), memoryStream),
             new TypedParameter(typeof(SharedFileDefinition), sharedFileDefinition),
             new TypedParameter(typeof(IFileUploadCoordinator), fileUploadCoordinator),
