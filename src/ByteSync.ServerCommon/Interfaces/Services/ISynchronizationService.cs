259534090c9cc7d9b65eb981c8aa0e997c56fb9a
Ghass-M <mastourighassen@gmail.com>
2025-07-31 07:42:22 +0100

    refactor: remove code duplication in SynchronizationService

--- src/ByteSync.ServerCommon/Interfaces/Services/ISynchronizationService.cs
+++ src/ByteSync.ServerCommon/Interfaces/Services/ISynchronizationService.cs
@@ -1,6 +1,4 @@
-﻿using ByteSync.Common.Business.Actions;
-using ByteSync.Common.Business.SharedFiles;
-using ByteSync.Common.Business.Synchronizations;
+﻿using ByteSync.Common.Business.SharedFiles;
 using ByteSync.ServerCommon.Business.Auth;
 using ByteSync.ServerCommon.Entities;
 
@@ -8,29 +6,11 @@ namespace ByteSync.ServerCommon.Interfaces.Services;
 
 public interface ISynchronizationService
 {
-    Task StartSynchronization(string sessionId, Client client, List<ActionsGroupDefinition> actionsGroupDefinitions);
-    
     Task OnUploadIsFinishedAsync(SharedFileDefinition sharedFileDefinition, int totalParts, Client client);
     
     Task OnFilePartIsUploadedAsync(SharedFileDefinition sharedFileDefinition, int partNumber);
     
     Task OnDownloadIsFinishedAsync(SharedFileDefinition sharedFileDefinition, Client client);
-    
-    Task OnDateIsCopied(string sessionId, List<string> actionsGroupIds, Client client);
-    
-    Task OnFileOrDirectoryIsDeletedAsync(string sessionId, List<string> actionsGroupIds, Client client);
-    
-    Task OnDirectoryIsCreatedAsync(string sessionId, List<string> actionsGroupIds, Client client);
-    
-    Task RequestAbortSynchronization(string sessionId, Client client);
-    
-    Task<Synchronization?> GetSynchronization(string sessionId, Client client);
-
-    Task OnLocalCopyIsDoneAsync(string sessionId, List<string> actionsGroupIds, Client client);
-    
-    Task AssertSynchronizationActionErrors(string sessionId, List<string> actionsGroupIds, Client client);
-
-    Task OnMemberHasFinished(string sessionId, Client client);
     
     Task ResetSession(string sessionId);
     
