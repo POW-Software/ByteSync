d0e163220560af2834b171577e8d6d7323ab2e44
Ghass-M <mastourighassen@gmail.com>
2025-08-08 17:44:14 +0100

    test: add integration tests

--- tests/ByteSync.Functions.IntegrationTests/Services/AzureBlobStorageService_Tests.cs
+++ tests/ByteSync.Functions.IntegrationTests/Services/AzureBlobStorageService_Tests.cs
@@ -1,5 +1,9 @@
 ﻿using Autofac;
 using ByteSync.ServerCommon.Services.Storage;
+using ByteSync.ServerCommon.Business.Settings;
+using Microsoft.Extensions.Options;
+using ByteSync.Common.Business.SharedFiles;
+using FluentAssertions;
 
 namespace ByteSync.Functions.IntegrationTests.Services;
 
@@ -23,7 +27,84 @@ public class AzureBlobStorageService_Tests
     [Test]
     public void TestWithSettings()
     {
-        var AzureBlobStoragService = _scope.Resolve<AzureBlobStorageService>();
-        
+        var azureBlobStorageService = _scope.Resolve<AzureBlobStorageService>();
+        azureBlobStorageService.Should().NotBeNull();
+        azureBlobStorageService.StorageSharedKeyCredential.Should().NotBeNull();
+    }
+
+    [Test]
+    public void StorageSharedKeyCredential_ShouldMatchConfiguredAccountName()
+    {
+        var options = _scope.Resolve<IOptions<AzureBlobStorageSettings>>();
+        var azureBlobStorageService = _scope.Resolve<AzureBlobStorageService>();
+
+        options.Value.Should().NotBeNull();
+        azureBlobStorageService.StorageSharedKeyCredential.AccountName.Should().Be(options.Value.AccountName);
+    }
+
+    [Test]
+    public async Task GetUploadAndDownloadUrls_ShouldContainSas_WithExpectedPermissions()
+    {
+        var service = _scope.Resolve<AzureBlobStorageService>();
+        var shared = new SharedFileDefinition
+        {
+            SessionId = "sess123",
+            ClientInstanceId = "cliABC",
+            SharedFileType = SharedFileTypes.FullSynchronization,
+            AdditionalName = "ignored"
+        };
+        int partNumber = 2;
+
+        var uploadUrl = await service.GetUploadFileUrl(shared, partNumber);
+        var downloadUrl = await service.GetDownloadFileUrl(shared, partNumber);
+
+        uploadUrl.Should().NotBeNullOrEmpty();
+        downloadUrl.Should().NotBeNullOrEmpty();
+
+        var uploadUri = new Uri(uploadUrl);
+        var downloadUri = new Uri(downloadUrl);
+
+        var uploadQuery = System.Web.HttpUtility.ParseQueryString(uploadUri.Query);
+        var downloadQuery = System.Web.HttpUtility.ParseQueryString(downloadUri.Query);
+        uploadQuery["se"].Should().NotBeNull(); // expiry
+        uploadQuery["sp"].Should().NotBeNull(); // permissions
+        downloadQuery["se"].Should().NotBeNull();
+        downloadQuery["sp"].Should().NotBeNull();
+
+        uploadQuery["sp"]!.Should().Contain("w");
+        downloadQuery["sp"]!.Should().Contain("r");
+    }
+
+    [Test]
+    public async Task Urls_ShouldDiffer_AndPartNumberImpactsKey()
+    {
+        var service = _scope.Resolve<AzureBlobStorageService>();
+        var shared = new SharedFileDefinition
+        {
+            SessionId = "sessA",
+            ClientInstanceId = "cliA",
+            SharedFileType = SharedFileTypes.FullSynchronization
+        };
+
+        var url1 = await service.GetUploadFileUrl(shared, 1);
+        var url2 = await service.GetUploadFileUrl(shared, 2);
+        url1.Should().NotBe(url2);
+    }
+
+    [Test]
+    public async Task InventoryType_ShouldUseAdditionalNameInKey()
+    {
+        var service = _scope.Resolve<AzureBlobStorageService>();
+        var shared = new SharedFileDefinition
+        {
+            SessionId = "sessInvA",
+            ClientInstanceId = "cliInvA",
+            SharedFileType = SharedFileTypes.BaseInventory,
+            AdditionalName = "profileABC"
+        };
+
+        var url = await service.GetDownloadFileUrl(shared, 1);
+        var uri = new Uri(url);
+        uri.ToString().Should().Contain("base_inventory_profileABC");
     }
 }
