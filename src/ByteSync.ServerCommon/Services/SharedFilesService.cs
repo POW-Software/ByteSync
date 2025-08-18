0ec156b36d6f3a047d99b783ac7d48c618a4972e
Ghass-M <mastourighassen@gmail.com>
2025-08-18 13:17:54 +0100

    feat: allow optional data suppression

--- src/ByteSync.ServerCommon/Services/SharedFilesService.cs
+++ src/ByteSync.ServerCommon/Services/SharedFilesService.cs
@@ -1,10 +1,12 @@
 ﻿using ByteSync.Common.Business.SharedFiles;
 using ByteSync.ServerCommon.Business.Auth;
 using ByteSync.ServerCommon.Business.Sessions;
+using ByteSync.ServerCommon.Business.Settings;
 using ByteSync.ServerCommon.Interfaces.Repositories;
 using ByteSync.ServerCommon.Interfaces.Services;
 using ByteSync.ServerCommon.Interfaces.Services.Storage;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace ByteSync.ServerCommon.Services;
 
@@ -13,19 +15,21 @@ public class SharedFilesService : ISharedFilesService
     private readonly ISharedFilesRepository _sharedFilesRepository;
     private readonly IAzureBlobStorageService _azureBlobStorageService;
     private readonly ICloudflareR2Service _cloudflareR2Service;
-
+    private readonly AppSettings _appSettings;
     private readonly ILogger<SharedFilesService> _logger;
 
     public SharedFilesService(
         ISharedFilesRepository sharedFilesRepository,
         IAzureBlobStorageService azureBlobStorageService,
         ICloudflareR2Service cloudflareR2Service,
-        ILogger<SharedFilesService> logger)
+        ILogger<SharedFilesService> logger,
+        IOptions<AppSettings> appSettings)
     {
         _sharedFilesRepository = sharedFilesRepository;
         _azureBlobStorageService = azureBlobStorageService;
         _cloudflareR2Service = cloudflareR2Service;
         _logger = logger;
+        _appSettings = appSettings.Value;
     }
     
     public async Task AssertFilePartIsUploaded(TransferParameters transferParameters, ICollection<string> recipients)
@@ -63,7 +67,7 @@ public class SharedFilesService : ISharedFilesService
     {
         var sharedFileDefinition = transferParameters.SharedFileDefinition;
         var partNumber = transferParameters.PartNumber!.Value;
-        bool deleteBlob = false;
+        bool objectDeletable = false;
         bool unregister = false;
         
         await _sharedFilesRepository.AddOrUpdate(sharedFileDefinition, sharedFileData =>
@@ -77,7 +81,7 @@ public class SharedFilesService : ISharedFilesService
             
             if (sharedFileData.IsPartFullyDownloaded(partNumber))
             {
-                deleteBlob = true;
+                objectDeletable = true;
             }
 
             if (sharedFileData.IsFullyDownloaded)
@@ -88,7 +92,7 @@ public class SharedFilesService : ISharedFilesService
             return sharedFileData;
         });
 
-        if (deleteBlob)
+        if ((objectDeletable) && (!_appSettings.RetainFilesAfterTransfer))
         {
             try
             {
