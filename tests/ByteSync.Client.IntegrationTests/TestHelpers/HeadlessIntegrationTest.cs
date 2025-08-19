8592a72bd4d2e99712148fb001bc5f7134f66dcf
Paul Fresquet <paul.fresquet@pow-software.com>
2025-08-19 08:51:38 +0200

    test: improve integration tests test: add FlyoutContainerViewModel_HeadlessTests

--- tests/ByteSync.Client.IntegrationTests/TestHelpers/HeadlessIntegrationTest.cs
+++ tests/ByteSync.Client.IntegrationTests/TestHelpers/HeadlessIntegrationTest.cs
@@ -6,12 +6,15 @@ using Avalonia.Threading;
 using ByteSync;
 using ByteSync.TestsCommon;
 using NUnit.Framework;
+using System.Diagnostics;
+using System.Threading;
 
 namespace ByteSync.Client.IntegrationTests.TestHelpers;
 
 public abstract class HeadlessIntegrationTest : IntegrationTest
 {
     private static bool _initialized;
+    // No need for a running lifetime; we will pump UI jobs manually
 
     [OneTimeSetUp]
     public static void GlobalSetup()
@@ -28,26 +31,60 @@ public abstract class HeadlessIntegrationTest : IntegrationTest
         _initialized = true;
     }
 
+    [OneTimeTearDown]
+    public static void GlobalTeardown()
+    {
+        _initialized = false;
+    }
+
     protected Task ExecuteOnUiThread(Func<Task> action)
     {
         var tcs = new TaskCompletionSource();
-        Dispatcher.UIThread.InvokeAsync(async () =>
+        Dispatcher.UIThread.Post(async () =>
         {
-            await action();
-            tcs.SetResult();
+            try
+            {
+                await action();
+                tcs.SetResult();
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
         });
+
+        PumpUiJobsUntilComplete(tcs.Task);
         return tcs.Task;
     }
 
     protected Task<T> ExecuteOnUiThread<T>(Func<Task<T>> action)
     {
         var tcs = new TaskCompletionSource<T>();
-        Dispatcher.UIThread.InvokeAsync(async () =>
+        Dispatcher.UIThread.Post(async () =>
         {
-            var result = await action();
-            tcs.SetResult(result);
+            try
+            {
+                var result = await action();
+                tcs.SetResult(result);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
         });
+
+        PumpUiJobsUntilComplete(tcs.Task);
         return tcs.Task;
+    }
+
+    private static void PumpUiJobsUntilComplete(Task task, int timeoutMs = 5000)
+    {
+        var sw = Stopwatch.StartNew();
+        while (!task.IsCompleted && sw.ElapsedMilliseconds < timeoutMs)
+        {
+            Dispatcher.UIThread.RunJobs();
+            Thread.Sleep(1);
+        }
     }
 
     private class TestApp : Application
---
+++ tests/ByteSync.Client.IntegrationTests/ViewModels/Misc/FlyoutContainerViewModel_HeadlessTests.cs
@@ -0,0 +1,68 @@
+using ByteSync.Client.IntegrationTests.TestHelpers;
+using ByteSync.Business;
+using ByteSync.Interfaces.Factories.ViewModels;
+using ByteSync.Interfaces.Services.Localizations;
+using ByteSync.ViewModels.Misc;
+using FluentAssertions;
+using Moq;
+
+namespace ByteSync.Client.IntegrationTests.ViewModels.Misc;
+
+public class FlyoutContainerViewModel_HeadlessTests : HeadlessIntegrationTest
+{
+    private Mock<ILocalizationService> _localizationServiceMock = null!;
+    private Mock<IFlyoutElementViewModelFactory> _factoryMock = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _localizationServiceMock = new Mock<ILocalizationService>();
+        _localizationServiceMock
+            .Setup(ls => ls[It.IsAny<string>()])
+            .Returns((string key) => $"{key}-title");
+
+        _factoryMock = new Mock<IFlyoutElementViewModelFactory>();
+    }
+
+    [Test]
+    public async Task ShowMessageBoxAsync_Should_Open_And_Return_OK()
+    {
+        var vm = new FlyoutContainerViewModel(_localizationServiceMock.Object, _factoryMock.Object);
+        vm.Activator.Activate();
+
+        var messageBox = new MessageBoxViewModel("Test.Title", null, null, _localizationServiceMock.Object);
+
+        // Start ShowMessageBoxAsync on UI thread but DO NOT await its completion here
+        // We need the returned Task so we can click OK later, otherwise we deadlock
+        var resultTask = await ExecuteOnUiThread(() => Task.FromResult(vm.ShowMessageBoxAsync(messageBox)));
+
+        // While waiting, ensure the flyout is shown and content is bound
+        await ExecuteOnUiThread(async () =>
+        {
+            vm.Content.Should().BeSameAs(messageBox);
+            vm.IsFlyoutContainerVisible.Should().BeTrue();
+            await Task.CompletedTask;
+        });
+
+        // Simulate user clicking OK on UI thread
+        await ExecuteOnUiThread(async () =>
+        {
+            messageBox.OKButtonCommand.Execute().Subscribe();
+            await Task.CompletedTask;
+        });
+
+        // Retrieve result
+        var result = await resultTask;
+        result.Should().Be(MessageBoxResult.OK);
+
+        // And the flyout should be closed
+        await ExecuteOnUiThread(async () =>
+        {
+            vm.IsFlyoutContainerVisible.Should().BeFalse();
+            vm.Content.Should().BeNull();
+            await Task.CompletedTask;
+        });
+    }
+}
+
+
