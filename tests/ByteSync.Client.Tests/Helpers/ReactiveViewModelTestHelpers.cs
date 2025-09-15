c92f4b2985adccfc1d153c3951fd0ca8db1ac6e9
Paul Fresquet <paul.fresquet@pow-software.com>
2025-09-15 06:49:18 +0200

    test: improve test output

--- tests/ByteSync.Client.Tests/Helpers/ReactiveViewModelTestHelpers.cs
+++ tests/ByteSync.Client.Tests/Helpers/ReactiveViewModelTestHelpers.cs
@@ -1,5 +1,4 @@
 using System.Linq.Expressions;
-using System.Reactive.Linq;
 using FluentAssertions;
 using ReactiveUI;
 
@@ -16,13 +15,34 @@ public static class ReactiveViewModelTestHelpers
         TimeSpan timeout)
         where TViewModel : class
     {
+        return WaitForProperty(viewModel, selector, predicate, timeout, out _);
+    }
+
+    public static bool WaitForProperty<TViewModel, TProp>(
+        TViewModel viewModel,
+        Expression<Func<TViewModel, TProp>> selector,
+        Func<TProp, bool> predicate,
+        TimeSpan timeout,
+        out TProp? lastValue)
+        where TViewModel : class
+    {
+        TProp latest = default!;
         var evt = new ManualResetEventSlim();
         using var sub = viewModel
             .WhenAnyValue(selector)
-            .Where(predicate)
-            .Subscribe(_ => evt.Set());
+            .Subscribe(v =>
+            {
+                latest = v;
+                if (predicate(v))
+                {
+                    evt.Set();
+                }
+            });
 
-        return evt.Wait(timeout);
+        var signaled = evt.Wait(timeout);
+        lastValue = latest;
+
+        return signaled;
     }
 
     public static void ShouldEventuallyBe<TViewModel, TProp>(
@@ -32,8 +52,10 @@ public static class ReactiveViewModelTestHelpers
         TimeSpan timeout)
         where TViewModel : class
     {
-        var ok = WaitForProperty(viewModel, selector, v => Equals(v, expected), timeout);
-        ok.Should().BeTrue();
+        var ok = WaitForProperty(viewModel, selector, v => Equals(v, expected), timeout, out var lastValue);
+        var member = GetMemberName(selector);
+        ok.Should().BeTrue(
+            $"Expected {member} to become '{expected}' within {timeout}, but last value was '{lastValue}'");
     }
 
     public static void ShouldEventuallyBe<TViewModel, TProp>(
@@ -43,5 +65,10 @@ public static class ReactiveViewModelTestHelpers
         where TViewModel : class
     {
         ShouldEventuallyBe(viewModel, selector, expected, _defaultTimeout);
+    }
+
+    private static string GetMemberName<TViewModel, TProp>(Expression<Func<TViewModel, TProp>> selector)
+    {
+        return selector.Body is MemberExpression m ? m.Member.Name : selector.ToString();
     }
 }
