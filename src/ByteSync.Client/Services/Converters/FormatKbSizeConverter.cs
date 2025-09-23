b7fb3a2110571c150be4ccd75d1efb0bc4681c77
Paul Fresquet <paul.fresquet@pow-software.com>
2025-09-23 07:14:41 +0200

    feat: improve ui

--- src/ByteSync.Client/Services/Converters/FormatKbSizeConverter.cs
+++ src/ByteSync.Client/Services/Converters/FormatKbSizeConverter.cs
@@ -3,8 +3,6 @@ using Autofac;
 using Avalonia.Controls;
 using Avalonia.Data.Converters;
 using ByteSync.Assets.Resources;
-using ByteSync.Common.Helpers;
-using ByteSync.Interfaces;
 using ByteSync.Interfaces.Converters;
 using ByteSync.Interfaces.Services.Localizations;
 
@@ -13,7 +11,7 @@ namespace ByteSync.Services.Converters;
 public class FormatKbSizeConverter : IValueConverter, IFormatKbSizeConverter
 {
     private readonly ILocalizationService _localizationService = null!;
-
+    
     public FormatKbSizeConverter()
     {
         if (!Design.IsDesignMode)
@@ -21,16 +19,21 @@ public class FormatKbSizeConverter : IValueConverter, IFormatKbSizeConverter
             _localizationService = ContainerProvider.Container.Resolve<ILocalizationService>();
         }
     }
-        
+    
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo? culture)
     {
         if (Design.IsDesignMode)
         {
             return new object();
         }
-            
+        
+        if (value is null)
+        {
+            return string.Empty;
+        }
+        
         var number = System.Convert.ToInt64(value);
-
+        
         string format = "N1";
         bool convertUntilTeraBytes = false;
         if (parameter is string sParameter && sParameter.Equals("true//N0", StringComparison.CurrentCultureIgnoreCase))
@@ -38,18 +41,19 @@ public class FormatKbSizeConverter : IValueConverter, IFormatKbSizeConverter
             // A parser plus tard
             parameter = new FormatKbSizeConverterParameters { ConvertUntilTeraBytes = true, Format = "N0" };
         }
+        
         if (parameter is FormatKbSizeConverterParameters formatKbSizeConverterParameters)
         {
             if (formatKbSizeConverterParameters.Format.IsNotEmpty(true))
             {
                 format = formatKbSizeConverterParameters.Format!;
             }
-
+            
             convertUntilTeraBytes = formatKbSizeConverterParameters.ConvertUntilTeraBytes;
         }
-    
+        
         string result;
-
+        
         if (convertUntilTeraBytes && number >= (long)1024 * 1024 * 1024 * 1024)
         {
             var preValue = (number / 1024.0 / 1024 / 1024 / 1024).ToString(format);
@@ -75,15 +79,15 @@ public class FormatKbSizeConverter : IValueConverter, IFormatKbSizeConverter
             var preValue = number;
             result = String.Format(_localizationService[nameof(Resources.Misc_SizeUnitTemplate_Byte)], preValue);
         }
-
+        
         return result;
     }
-
+    
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return null;
     }
-
+    
     public string Convert(long? value)
     {
         return (string)Convert(value, typeof(string), null, null);
@@ -93,6 +97,6 @@ public class FormatKbSizeConverter : IValueConverter, IFormatKbSizeConverter
 public class FormatKbSizeConverterParameters
 {
     public string? Format { get; set; }
-        
+    
     public bool ConvertUntilTeraBytes { get; set; }
 }
