c9f19286b7c332e2d89b4c076eeb2acea5453cdb
Paul Fresquet <paul.fresquet@pow-software.com>
2025-09-27 10:23:21 +0200

    feat: add icon & status for local inventory

--- src/ByteSync.Client/ViewModels/Sessions/Inventories/InventoryLocalStatusViewModel.cs
+++ src/ByteSync.Client/ViewModels/Sessions/Inventories/InventoryLocalStatusViewModel.cs
@@ -1,9 +1,11 @@
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using Avalonia.Media;
 using ByteSync.Assets.Resources;
 using ByteSync.Business.Inventories;
 using ByteSync.Business.Misc;
 using ByteSync.Interfaces.Controls.Inventories;
+using ByteSync.Interfaces.Controls.Themes;
 using ByteSync.Interfaces.Controls.TimeTracking;
 using ByteSync.Interfaces.Services.Sessions;
 using ReactiveUI;
@@ -16,18 +18,28 @@ public class InventoryLocalStatusViewModel : ActivatableViewModelBase
     private readonly ISessionService _sessionService = null!;
     private readonly ITimeTrackingCache _timeTrackingCache = null!;
     private readonly IInventoryService _inventoryService = null!;
+    private readonly IThemeService? _themeService;
     
     public InventoryLocalStatusViewModel()
     {
         EstimatedProcessEndName = Resources.InventoryProcess_EstimatedEnd;
+        LocalMainIcon = "None";
+        LocalMainStatusText = string.Empty;
+        LocalMainIconBrush = null;
     }
     
     public InventoryLocalStatusViewModel(ISessionService sessionService, ITimeTrackingCache timeTrackingCache,
-        IInventoryService inventoryService) : this()
+        IInventoryService inventoryService) : this(sessionService, timeTrackingCache, inventoryService, null)
+    {
+    }
+    
+    public InventoryLocalStatusViewModel(ISessionService sessionService, ITimeTrackingCache timeTrackingCache,
+        IInventoryService inventoryService, IThemeService? themeService) : this()
     {
         _sessionService = sessionService;
         _timeTrackingCache = timeTrackingCache;
         _inventoryService = inventoryService;
+        _themeService = themeService;
         
         this.WhenActivated(disposables =>
         {
@@ -52,6 +64,70 @@ public class InventoryLocalStatusViewModel : ActivatableViewModelBase
                 .ObserveOn(RxApp.MainThreadScheduler)
                 .Subscribe(text => EstimatedProcessEndName = text)
                 .DisposeWith(disposables);
+            
+            var statusStream = _inventoryService.InventoryProcessData.MainStatus
+                .DistinctUntilChanged()
+                .Replay(1)
+                .RefCount();
+            
+            statusStream
+                .Select(st => st is InventoryTaskStatus.Pending or InventoryTaskStatus.Running)
+                .DistinctUntilChanged()
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .ToPropertyEx(this, x => x.IsLocalInventoryInProgress)
+                .DisposeWith(disposables);
+            
+            statusStream
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(st =>
+                {
+                    switch (st)
+                    {
+                        case InventoryTaskStatus.Pending:
+                        case InventoryTaskStatus.Running:
+                            LocalMainIcon = "None";
+                            LocalMainStatusText =
+                                Resources.ResourceManager.GetString("InventoryProcess_LocalInventory_Running", Resources.Culture)
+                                ?? Resources.InventoryProcess_InventoryRunning;
+                            LocalMainIconBrush = null;
+                            
+                            break;
+                        case InventoryTaskStatus.Success:
+                            LocalMainIcon = "SolidCheckCircle";
+                            LocalMainStatusText =
+                                Resources.ResourceManager.GetString("InventoryProcess_LocalInventory_Success", Resources.Culture)
+                                ?? Resources.InventoryProcess_InventorySuccess;
+                            LocalMainIconBrush = _themeService?.GetBrush("HomeCloudSynchronizationBackGround");
+                            
+                            break;
+                        case InventoryTaskStatus.Cancelled:
+                            LocalMainIcon = "SolidXCircle";
+                            LocalMainStatusText =
+                                Resources.ResourceManager.GetString("InventoryProcess_LocalInventory_Cancelled", Resources.Culture)
+                                ?? Resources.InventoryProcess_InventoryCancelled;
+                            LocalMainIconBrush = _themeService?.GetBrush("MainSecondaryColor");
+                            
+                            break;
+                        case InventoryTaskStatus.Error:
+                            LocalMainIcon = "SolidXCircle";
+                            LocalMainStatusText =
+                                Resources.ResourceManager.GetString("InventoryProcess_LocalInventory_Error", Resources.Culture)
+                                ?? Resources.InventoryProcess_InventoryError;
+                            LocalMainIconBrush = _themeService?.GetBrush("MainSecondaryColor");
+                            
+                            break;
+                        case InventoryTaskStatus.NotLaunched:
+                        default:
+                            LocalMainIcon = "SolidXCircle";
+                            LocalMainStatusText =
+                                Resources.ResourceManager.GetString("InventoryProcess_LocalInventory_NotLaunched", Resources.Culture)
+                                ?? Resources.InventoryProcess_InventoryError;
+                            LocalMainIconBrush = _themeService?.GetBrush("MainSecondaryColor");
+                            
+                            break;
+                    }
+                })
+                .DisposeWith(disposables);
         });
     }
     
@@ -69,4 +145,15 @@ public class InventoryLocalStatusViewModel : ActivatableViewModelBase
     
     [Reactive]
     public TimeSpan? RemainingTime { get; set; }
+    
+    public extern bool IsLocalInventoryInProgress { [ObservableAsProperty] get; }
+    
+    [Reactive]
+    public string LocalMainIcon { get; set; }
+    
+    [Reactive]
+    public string LocalMainStatusText { get; set; }
+    
+    [Reactive]
+    public IBrush? LocalMainIconBrush { get; set; }
 }
