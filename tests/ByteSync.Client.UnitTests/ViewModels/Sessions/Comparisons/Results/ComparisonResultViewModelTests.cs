f301fd54726b968f0ba58e88f67bc7f57a0dbfd6
Paul Fresquet <paul.fresquet@pow-software.com>
2025-10-05 21:57:43 +0200

    test: add tests

--- tests/ByteSync.Client.UnitTests/ViewModels/Sessions/Comparisons/Results/ComparisonResultViewModelTests.cs
+++ tests/ByteSync.Client.UnitTests/ViewModels/Sessions/Comparisons/Results/ComparisonResultViewModelTests.cs
@@ -1,11 +1,17 @@
-using System.Reactive.Subjects;
-using Autofac;
 using System.Reactive.Linq;
-using ByteSync.Business;
-using ByteSync.Business.Sessions;
+using System.Reactive.Subjects;
+using System.Reflection;
+using Avalonia.Media;
+using ByteSync.Business.Filtering.Parsing;
 using ByteSync.Business.Inventories;
+using ByteSync.Business.Sessions;
+using ByteSync.Business.Themes;
+using ByteSync.Common.Business.EndPoints;
+using ByteSync.Common.Business.Inventories;
 using ByteSync.Common.Business.Misc;
+using ByteSync.Interfaces.Controls.Communications;
 using ByteSync.Interfaces.Controls.Inventories;
+using ByteSync.Interfaces.Controls.Themes;
 using ByteSync.Interfaces.Dialogs;
 using ByteSync.Interfaces.Factories.ViewModels;
 using ByteSync.Interfaces.Repositories;
@@ -16,11 +22,11 @@ using ByteSync.Models.Comparisons.Result;
 using ByteSync.Models.Inventories;
 using ByteSync.Repositories;
 using ByteSync.ViewModels.Sessions.Comparisons.Results;
+using ByteSync.Views.Misc;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
-using ByteSync.Interfaces.Controls.Communications;
 
 namespace ByteSync.Client.UnitTests.ViewModels.Sessions.Comparisons.Results;
 
@@ -39,32 +45,32 @@ public class ComparisonResultViewModelTests
     private Mock<IFilterService> _filterService = null!;
     private Mock<IWebAccessor> _webAccessor = null!;
     private Mock<ILogger<ComparisonResultViewModel>> _logger = null!;
-
+    
     private ReplaySubject<ComparisonResult?> _resultSubject = null!;
-
+    
     [SetUp]
     public void Setup()
     {
         _sessionService = new Mock<ISessionService>();
         _sessionService.SetupGet(s => s.CurrentSessionSettings).Returns(SessionSettings.BuildDefault());
-        _sessionService.SetupGet(s => s.SessionStatusObservable).Returns(System.Reactive.Linq.Observable.Never<SessionStatus>());
-
+        _sessionService.SetupGet(s => s.SessionStatusObservable).Returns(Observable.Never<SessionStatus>());
+        
         _localizationService = new Mock<ILocalizationService>();
         _localizationService.Setup(ls => ls[It.IsAny<string>()]).Returns((string key) => key);
-
+        
         _dialogService = new Mock<IDialogService>();
         _inventoryService = new Mock<IInventoryService>();
-        _inventoryService.SetupGet(s => s.InventoryProcessData).Returns(new ByteSync.Business.Inventories.InventoryProcessData());
-
+        _inventoryService.SetupGet(s => s.InventoryProcessData).Returns(new InventoryProcessData());
+        
         _comparisonItemsService = new Mock<IComparisonItemsService>();
         _resultSubject = new ReplaySubject<ComparisonResult?>(1);
         _comparisonItemsService.SetupProperty(s => s.ComparisonResult, _resultSubject);
         _comparisonItemsService.SetupGet(s => s.ComparisonResult).Returns(_resultSubject);
-
+        
         _comparisonItemViewModelFactory = new Mock<IComparisonItemViewModelFactory>();
         _sessionMemberRepository = new Mock<ISessionMemberRepository>();
         _sessionMemberRepository.SetupGet(r => r.IsCurrentUserFirstSessionMemberCurrentValue).Returns(true);
-
+        
         _flyoutElementViewModelFactory = new Mock<IFlyoutElementViewModelFactory>();
         _comparisonItemRepository = new ComparisonItemRepository();
         _filterService = new Mock<IFilterService>();
@@ -72,7 +78,7 @@ public class ComparisonResultViewModelTests
         _webAccessor = new Mock<IWebAccessor>();
         _logger = new Mock<ILogger<ComparisonResultViewModel>>();
     }
-
+    
     private static Inventory BuildInventory(string code, string ci, string machine)
     {
         return new Inventory
@@ -80,14 +86,14 @@ public class ComparisonResultViewModelTests
             InventoryId = $"INV_{code}",
             Code = code,
             MachineName = machine,
-            Endpoint = new ByteSync.Common.Business.EndPoints.ByteSyncEndpoint
+            Endpoint = new ByteSyncEndpoint
             {
                 ClientInstanceId = ci,
                 OSPlatform = OSPlatforms.Windows
             }
         };
     }
-
+    
     [Test]
     public void WhenComparisonResultArrives_ColumnsVisibilityAndNamesAreSet()
     {
@@ -105,23 +111,98 @@ public class ComparisonResultViewModelTests
             _filterService.Object,
             _webAccessor.Object,
             _logger.Object);
-
+        
         vm.Activator.Activate();
-
+        
         var result = new ComparisonResult();
         result.AddInventory(BuildInventory("Aa", "CII_A", "POW-25-EB01"));
         result.AddInventory(BuildInventory("Ba", "CII_B", "POW-25-EB01"));
         result.AddInventory(BuildInventory("Bb", "CII_B", "POW-25-EB01"));
-
+        
         _resultSubject.OnNext(result);
-
+        
         vm.AreResultsLoaded.Should().BeTrue();
         vm.IsColumn2Visible.Should().BeTrue();
         vm.IsColumn3Visible.Should().BeTrue();
         vm.IsColumn4Visible.Should().BeFalse();
-
+        
         vm.Inventory1Name.Should().Contain("Aa");
         vm.Inventory2Name.Should().Contain("Ba");
         vm.Inventory3Name.Should().Contain("Bb");
+    }
+    
+    [Test]
+    public void BuildFilter_NoTags_ReturnsAlwaysTrue_AndDoesNotCallFilterService()
+    {
+        var vm = new ComparisonResultViewModel(
+            _sessionService.Object,
+            _localizationService.Object,
+            _dialogService.Object,
+            _inventoryService.Object,
+            _comparisonItemsService.Object,
+            _comparisonItemViewModelFactory.Object,
+            _sessionMemberRepository.Object,
+            _flyoutElementViewModelFactory.Object,
+            new ManageSynchronizationRulesViewModel(),
+            _comparisonItemRepository,
+            _filterService.Object,
+            _webAccessor.Object,
+            _logger.Object);
+        
+        var buildFilter = typeof(ComparisonResultViewModel).GetMethod("BuildFilter", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        var predicate = (Func<ComparisonItem, bool>)buildFilter.Invoke(vm, null)!;
+        
+        var item1 = new ComparisonItem(new PathIdentity(FileSystemTypes.File, "/a", "a", "/a"));
+        var item2 = new ComparisonItem(new PathIdentity(FileSystemTypes.File, "/b", "b", "/b"));
+        
+        predicate(item1).Should().BeTrue();
+        predicate(item2).Should().BeTrue();
+        _filterService.Verify(f => f.BuildFilter(It.IsAny<List<string>>()), Times.Never);
+    }
+    
+    [Test]
+    public void BuildFilter_WithTags_DelegatesToFilterService_WithExtractedTexts()
+    {
+        // Arrange filter service to build a predicate based on provided tags
+        _filterService
+            .Setup(f => f.BuildFilter(It.IsAny<List<string>>()))
+            .Returns<List<string>>(tags => (ComparisonItem ci) =>
+                tags.All(t => ci.PathIdentity.LinkingKeyValue.Contains(t, StringComparison.OrdinalIgnoreCase)));
+        
+        var vm = new ComparisonResultViewModel(
+            _sessionService.Object,
+            _localizationService.Object,
+            _dialogService.Object,
+            _inventoryService.Object,
+            _comparisonItemsService.Object,
+            _comparisonItemViewModelFactory.Object,
+            _sessionMemberRepository.Object,
+            _flyoutElementViewModelFactory.Object,
+            new ManageSynchronizationRulesViewModel(),
+            _comparisonItemRepository,
+            _filterService.Object,
+            _webAccessor.Object,
+            _logger.Object);
+        
+        // Inject one TagItem with text "foo"
+        var filterParser = new Mock<IFilterParser>();
+        filterParser.Setup(p => p.TryParse(It.IsAny<string>())).Returns(ParseResult.Incomplete(""));
+        var themeSvc = new Mock<IThemeService>();
+        themeSvc.SetupGet(t => t.SelectedTheme).Returns(Observable.Never<Theme>());
+        themeSvc.Setup(t => t.GetBrush(It.IsAny<string>())).Returns((IBrush?)null);
+        
+        vm.FilterTags.Add(new TagItem(filterParser.Object, themeSvc.Object, "foo"));
+        
+        var buildFilter = typeof(ComparisonResultViewModel).GetMethod("BuildFilter", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        var predicate = (Func<ComparisonItem, bool>)buildFilter.Invoke(vm, null)!;
+        
+        // Two items: only one contains tag "foo" in linking key value
+        var match = new ComparisonItem(new PathIdentity(FileSystemTypes.File, "/dir/foo.txt", "foo.txt", "/dir/foo.txt"));
+        var miss = new ComparisonItem(new PathIdentity(FileSystemTypes.File, "/dir/bar.txt", "bar.txt", "/dir/bar.txt"));
+        
+        predicate(match).Should().BeTrue();
+        predicate(miss).Should().BeFalse();
+        
+        _filterService.Verify(f => f.BuildFilter(It.Is<List<string>>(l => l.Count == 1 && l[0] == "foo")), Times.Once);
     }
 }
