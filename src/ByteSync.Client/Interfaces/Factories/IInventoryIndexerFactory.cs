0184a75845ff7a4df9d0a9d49d332893227989a6
Paul Fresquet <paul.fresquet@pow-software.com>
2025-10-09 13:28:17 +0200

    refactor: cleanup

--- src/ByteSync.Client/Interfaces/Factories/IInventoryIndexerFactory.cs
+++
@@ -1,9 +0,0 @@
-// using ByteSync.Services.Inventories;
-//
-// namespace ByteSync.Interfaces.Factories;
-//
-// public interface IInventoryIndexerFactory
-// {
-//     InventoryIndexer Create();
-// }
-
