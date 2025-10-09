4add0980312c087ad7cec8b5ba8c8cbb9cbe9e86
Paul Fresquet <paul.fresquet@pow-software.com>
2025-10-09 13:25:24 +0200

    test: FullInventoryRunner_IntegrationTests, one test OK

--- tests/ByteSync.Client.IntegrationTests/Services/Inventories/FullInventoryRunner_IntegrationTests.cs
+++ tests/ByteSync.Client.IntegrationTests/Services/Inventories/FullInventoryRunner_IntegrationTests.cs
@@ -9,7 +9,6 @@ using ByteSync.Business.Sessions;
 using ByteSync.Client.IntegrationTests.TestHelpers;
 using ByteSync.Common.Business.Inventories;
 using ByteSync.Common.Business.Misc;
-using ByteSync.Common.Business.Sessions;
 using ByteSync.Common.Business.SharedFiles;
 using ByteSync.Common.Helpers;
 using ByteSync.Interfaces;
@@ -135,214 +134,6 @@ public class FullInventoryRunner_IntegrationTests : IntegrationTest
     }
     
     [Test]
-    public async Task RunFullInventory_WithSingleDataNode_SuccessfullyAnalyzesFiles()
-    {
-        var sourceDir = CreateTestDirectory("source1");
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file1.txt", "content1");
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file2.txt", "content2 different");
-        
-        var dataNode = CreateDataNode("node1", "A");
-        _dataNodeRepository.AddOrUpdate(new[] { dataNode });
-        
-        var dataSource = new DataSource
-            { Id = Guid.NewGuid().ToString(), DataNodeId = dataNode.Id, Path = sourceDir.FullName, Code = "A1" };
-        _dataSourceRepository.AddOrUpdate(new[] { dataSource });
-        
-        var inventoryBuilder = CreateInventoryBuilder(dataNode, new[] { dataSource });
-        
-        var baseInventoryPath = _cloudSessionLocalDataManager.GetCurrentMachineInventoryPath(
-            inventoryBuilder.Inventory, LocalInventoryModes.Base);
-        await inventoryBuilder.BuildBaseInventoryAsync(baseInventoryPath);
-        
-        await _inventoryService.SetLocalInventory(
-            new[] { new InventoryFile(BuildSharedFileDefinition(inventoryBuilder.Inventory, LocalInventoryModes.Base), baseInventoryPath) },
-            LocalInventoryModes.Base);
-        
-        await Task.Delay(100);
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file3.txt", "new content");
-        File.WriteAllText(Path.Combine(sourceDir.FullName, "file1.txt"), "modified content1");
-        
-        SetupInventoryComparerMock(inventoryBuilder, new List<string> { "/file1.txt", "/file3.txt" });
-        
-        _inventoryProcessData.InventoryBuilders = new List<IInventoryBuilder> { inventoryBuilder };
-        
-        InventoryTaskStatus? analysisStatus = null;
-        _inventoryProcessData.AnalysisStatus.Subscribe(s => analysisStatus = s);
-        
-        var result = await _fullInventoryRunner.RunFullInventory();
-        
-        result.Should().BeTrue();
-        analysisStatus.Should().Be(InventoryTaskStatus.Success);
-        _sessionMemberServiceMock.Verify(
-            x => x.UpdateCurrentMemberGeneralStatus(SessionMemberGeneralStatus.InventoryRunningAnalysis),
-            Times.Once);
-        _inventoryFinishedServiceMock.Verify(
-            x => x.SetLocalInventoryFinished(It.IsAny<List<Inventory>>(), LocalInventoryModes.Full),
-            Times.Once);
-        
-        var monitorData1 = await _inventoryProcessData.InventoryMonitorObservable.FirstAsync();
-        monitorData1.AnalyzedFiles.Should().BeGreaterThan(0);
-    }
-    
-    [Test]
-    public async Task RunFullInventory_WithMultipleDataNodes_ProcessesInParallel()
-    {
-        var sourceDir1 = CreateTestDirectory("source1");
-        _testDirectoryService.CreateFileInDirectory(sourceDir1.FullName, "file1a.txt", "content1a");
-        _testDirectoryService.CreateFileInDirectory(sourceDir1.FullName, "file1b.txt", "content1b different");
-        
-        var sourceDir2 = CreateTestDirectory("source2");
-        _testDirectoryService.CreateFileInDirectory(sourceDir2.FullName, "file2a.txt", "content2a");
-        _testDirectoryService.CreateFileInDirectory(sourceDir2.FullName, "file2b.txt", "content2b unique");
-        
-        var dataNode1 = CreateDataNode("node1", "A");
-        var dataNode2 = CreateDataNode("node2", "B");
-        _dataNodeRepository.AddOrUpdate(new[] { dataNode1, dataNode2 });
-        
-        var dataSource1 = new DataSource
-            { Id = Guid.NewGuid().ToString(), DataNodeId = dataNode1.Id, Path = sourceDir1.FullName, Code = "A1" };
-        var dataSource2 = new DataSource
-            { Id = Guid.NewGuid().ToString(), DataNodeId = dataNode2.Id, Path = sourceDir2.FullName, Code = "B1" };
-        _dataSourceRepository.AddOrUpdate(new[] { dataSource1, dataSource2 });
-        
-        var inventoryBuilder1 = CreateInventoryBuilder(dataNode1, new[] { dataSource1 });
-        var inventoryBuilder2 = CreateInventoryBuilder(dataNode2, new[] { dataSource2 });
-        
-        var baseInventoryPath1 = _cloudSessionLocalDataManager.GetCurrentMachineInventoryPath(
-            inventoryBuilder1.Inventory, LocalInventoryModes.Base);
-        var baseInventoryPath2 = _cloudSessionLocalDataManager.GetCurrentMachineInventoryPath(
-            inventoryBuilder2.Inventory, LocalInventoryModes.Base);
-        
-        await inventoryBuilder1.BuildBaseInventoryAsync(baseInventoryPath1);
-        await inventoryBuilder2.BuildBaseInventoryAsync(baseInventoryPath2);
-        
-        await _inventoryService.SetLocalInventory(
-            new[]
-            {
-                new InventoryFile(BuildSharedFileDefinition(inventoryBuilder1.Inventory, LocalInventoryModes.Base), baseInventoryPath1),
-                new InventoryFile(BuildSharedFileDefinition(inventoryBuilder2.Inventory, LocalInventoryModes.Base), baseInventoryPath2)
-            },
-            LocalInventoryModes.Base);
-        
-        await Task.Delay(100);
-        _testDirectoryService.CreateFileInDirectory(sourceDir1.FullName, "file1c.txt", "new content");
-        File.WriteAllText(Path.Combine(sourceDir1.FullName, "file1a.txt"), "modified content");
-        _testDirectoryService.CreateFileInDirectory(sourceDir2.FullName, "file2c.txt", "new content");
-        File.WriteAllText(Path.Combine(sourceDir2.FullName, "file2a.txt"), "modified content");
-        
-        SetupInventoryComparerMock(inventoryBuilder1, new List<string> { "/file1a.txt", "/file1c.txt" });
-        
-        _inventoryProcessData.InventoryBuilders = new List<IInventoryBuilder> { inventoryBuilder1, inventoryBuilder2 };
-        
-        var result = await _fullInventoryRunner.RunFullInventory();
-        
-        result.Should().BeTrue();
-        var monitorData2 = await _inventoryProcessData.InventoryMonitorObservable.FirstAsync();
-        monitorData2.AnalyzedFiles.Should().BeGreaterThan(0);
-        _inventoryFinishedServiceMock.Verify(
-            x => x.SetLocalInventoryFinished(It.IsAny<List<Inventory>>(), LocalInventoryModes.Full),
-            Times.Once);
-    }
-    
-    [Test]
-    public async Task RunFullInventory_WhenCancelled_UpdatesStatusToCancelled()
-    {
-        var sourceDir = CreateTestDirectory("source1");
-        for (int i = 0; i < 50; i++)
-        {
-            _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, $"file{i}.txt", $"content{i} unique content for diff");
-        }
-        
-        var dataNode = CreateDataNode("node1", "A");
-        _dataNodeRepository.AddOrUpdate(new[] { dataNode });
-        
-        var dataSource = new DataSource
-            { Id = Guid.NewGuid().ToString(), DataNodeId = dataNode.Id, Path = sourceDir.FullName, Code = "A1" };
-        _dataSourceRepository.AddOrUpdate(new[] { dataSource });
-        
-        var inventoryBuilder = CreateInventoryBuilder(dataNode, new[] { dataSource });
-        
-        var baseInventoryPath = _cloudSessionLocalDataManager.GetCurrentMachineInventoryPath(
-            inventoryBuilder.Inventory, LocalInventoryModes.Base);
-        await inventoryBuilder.BuildBaseInventoryAsync(baseInventoryPath);
-        
-        await _inventoryService.SetLocalInventory(
-            new[] { new InventoryFile(BuildSharedFileDefinition(inventoryBuilder.Inventory, LocalInventoryModes.Base), baseInventoryPath) },
-            LocalInventoryModes.Base);
-        
-        await Task.Delay(100);
-        var filesToAnalyze = new List<string>();
-        for (int i = 0; i < 50; i++)
-        {
-            _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, $"file{i}_new.txt", $"new content{i} unique");
-            filesToAnalyze.Add($"/file{i}_new.txt");
-        }
-        
-        SetupInventoryComparerMock(inventoryBuilder, filesToAnalyze);
-        
-        _inventoryProcessData.InventoryBuilders = new List<IInventoryBuilder> { inventoryBuilder };
-        
-        InventoryTaskStatus? analysisStatus = null;
-        _inventoryProcessData.AnalysisStatus.Subscribe(s => analysisStatus = s);
-        
-        _inventoryProcessData.CancellationTokenSource.Cancel();
-        
-        var result = await _fullInventoryRunner.RunFullInventory();
-        
-        result.Should().BeTrue();
-        analysisStatus.Should().Be(InventoryTaskStatus.Cancelled);
-        _inventoryFinishedServiceMock.Verify(
-            x => x.SetLocalInventoryFinished(It.IsAny<List<Inventory>>(), LocalInventoryModes.Full),
-            Times.Never);
-    }
-    
-    [Test]
-    public async Task RunFullInventory_WhenExceptionOccurs_UpdatesStatusToError()
-    {
-        var sourceDir = CreateTestDirectory("source1");
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file1.txt", "content1");
-        
-        var dataNode = CreateDataNode("node1", "A");
-        _dataNodeRepository.AddOrUpdate(new[] { dataNode });
-        
-        var dataSource = new DataSource
-            { Id = Guid.NewGuid().ToString(), DataNodeId = dataNode.Id, Path = sourceDir.FullName, Code = "A1" };
-        _dataSourceRepository.AddOrUpdate(new[] { dataSource });
-        
-        var inventoryBuilder = CreateInventoryBuilder(dataNode, new[] { dataSource });
-        
-        var baseInventoryPath = _cloudSessionLocalDataManager.GetCurrentMachineInventoryPath(
-            inventoryBuilder.Inventory, LocalInventoryModes.Base);
-        await inventoryBuilder.BuildBaseInventoryAsync(baseInventoryPath);
-        
-        await _inventoryService.SetLocalInventory(
-            new[] { new InventoryFile(BuildSharedFileDefinition(inventoryBuilder.Inventory, LocalInventoryModes.Base), baseInventoryPath) },
-            LocalInventoryModes.Base);
-        
-        await Task.Delay(100);
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file2.txt", "new content");
-        File.WriteAllText(Path.Combine(sourceDir.FullName, "file1.txt"), "modified content");
-        
-        SetupInventoryComparerMock(inventoryBuilder, new List<string> { "/file1.txt", "/file2.txt" });
-        
-        _inventoryFinishedServiceMock.Setup(x => x.SetLocalInventoryFinished(
-                It.IsAny<List<Inventory>>(),
-                It.IsAny<LocalInventoryModes>()))
-            .ThrowsAsync(new InvalidOperationException("Test exception"));
-        
-        _inventoryProcessData.InventoryBuilders = new List<IInventoryBuilder> { inventoryBuilder };
-        
-        Exception? capturedException = null;
-        _inventoryProcessData.ErrorEvent.Subscribe(_ => capturedException = _inventoryProcessData.LastException);
-        
-        var result = await _fullInventoryRunner.RunFullInventory();
-        
-        result.Should().BeFalse();
-        capturedException.Should().NotBeNull();
-        capturedException.Should().BeOfType<InvalidOperationException>();
-    }
-    
-    [Test]
     public async Task RunFullInventory_WithNoFilesToAnalyze_CompletesSuccessfully()
     {
         var sourceDir = CreateTestDirectory("source1");
@@ -379,51 +170,6 @@ public class FullInventoryRunner_IntegrationTests : IntegrationTest
             Times.Once);
     }
     
-    [Test]
-    public async Task RunFullInventory_UpdatesMonitorDataCorrectly()
-    {
-        var sourceDir = CreateTestDirectory("source1");
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file1.txt", "content1");
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file2.txt", "content2 different");
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file3.txt", "content3 also different");
-        
-        var dataNode = CreateDataNode("node1", "A");
-        _dataNodeRepository.AddOrUpdate(new[] { dataNode });
-        
-        var dataSource = new DataSource
-            { Id = Guid.NewGuid().ToString(), DataNodeId = dataNode.Id, Path = sourceDir.FullName, Code = "A1" };
-        _dataSourceRepository.AddOrUpdate(new[] { dataSource });
-        
-        var inventoryBuilder = CreateInventoryBuilder(dataNode, new[] { dataSource });
-        
-        var baseInventoryPath = _cloudSessionLocalDataManager.GetCurrentMachineInventoryPath(
-            inventoryBuilder.Inventory, LocalInventoryModes.Base);
-        await inventoryBuilder.BuildBaseInventoryAsync(baseInventoryPath);
-        
-        await _inventoryService.SetLocalInventory(
-            new[] { new InventoryFile(BuildSharedFileDefinition(inventoryBuilder.Inventory, LocalInventoryModes.Base), baseInventoryPath) },
-            LocalInventoryModes.Base);
-        
-        await Task.Delay(100);
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file4.txt", "new content");
-        _testDirectoryService.CreateFileInDirectory(sourceDir.FullName, "file5.txt", "another new content");
-        File.WriteAllText(Path.Combine(sourceDir.FullName, "file1.txt"), "modified content1");
-        File.WriteAllText(Path.Combine(sourceDir.FullName, "file2.txt"), "modified content2");
-        
-        SetupInventoryComparerMock(inventoryBuilder, new List<string> { "/file1.txt", "/file2.txt", "/file4.txt", "/file5.txt" });
-        
-        _inventoryProcessData.InventoryBuilders = new List<IInventoryBuilder> { inventoryBuilder };
-        
-        var initialMonitorData = await _inventoryProcessData.InventoryMonitorObservable.FirstAsync();
-        var initialAnalyzableFiles = initialMonitorData.AnalyzableFiles;
-        
-        await _fullInventoryRunner.RunFullInventory();
-        
-        var finalMonitorData = await _inventoryProcessData.InventoryMonitorObservable.FirstAsync();
-        finalMonitorData.AnalyzableFiles.Should().BeGreaterThan(initialAnalyzableFiles);
-        finalMonitorData.AnalyzedFiles.Should().BeGreaterThan(0);
-    }
-    
     private void SetupInventoryComparerMock(IInventoryBuilder inventoryBuilder, List<string> filesToAnalyze)
     {
         var comparerMock = new Mock<IInventoryComparer>();
@@ -431,23 +177,12 @@ public class FullInventoryRunner_IntegrationTests : IntegrationTest
         var comparisonResult = new ComparisonResult();
         comparisonResult.Inventories.Add(inventoryBuilder.Inventory);
         
+        var inventoryPart = inventoryBuilder.Inventory.InventoryParts.First();
+        
         foreach (var fileRelativePath in filesToAnalyze)
         {
             var pathIdentity = new PathIdentity(FileSystemTypes.File, fileRelativePath, Path.GetFileName(fileRelativePath),
                 fileRelativePath);
-            var comparisonItem = new ComparisonItem(pathIdentity);
-            
-            var contentIdentity1 = new ContentIdentity(new ContentIdentityCore { Size = 100 });
-            var contentIdentity2 = new ContentIdentity(new ContentIdentityCore { Size = 200 });
-            
-            var inventoryPart = inventoryBuilder.Inventory.InventoryParts.First();
-            contentIdentity1.InventoryPartsByLastWriteTimes[DateTime.UtcNow] = new HashSet<InventoryPart> { inventoryPart };
-            
-            comparisonItem.AddContentIdentity(contentIdentity1);
-            comparisonItem.AddContentIdentity(contentIdentity2);
-            
-            comparisonResult.AddItem(comparisonItem);
-            
             var fileInfo = new FileInfo(IOUtils.Combine(inventoryPart.RootPath, fileRelativePath.TrimStart('/', '\\')));
             var fileDescription = new FileDescription
             {
@@ -456,6 +191,18 @@ public class FullInventoryRunner_IntegrationTests : IntegrationTest
                 Size = fileInfo.Exists ? fileInfo.Length : 0
             };
             inventoryBuilder.Indexer.Register(fileDescription, pathIdentity);
+            
+            var comparisonItem = new ComparisonItem(pathIdentity);
+            
+            var contentIdentity1 = new ContentIdentity(new ContentIdentityCore { Size = 100 });
+            var contentIdentity2 = new ContentIdentity(new ContentIdentityCore { Size = 200 });
+            
+            contentIdentity1.InventoryPartsByLastWriteTimes[DateTime.UtcNow] = new HashSet<InventoryPart> { inventoryPart };
+            
+            comparisonItem.AddContentIdentity(contentIdentity1);
+            comparisonItem.AddContentIdentity(contentIdentity2);
+            
+            comparisonResult.AddItem(comparisonItem);
         }
         
         comparerMock.Setup(x => x.Compare()).Returns(comparisonResult);
