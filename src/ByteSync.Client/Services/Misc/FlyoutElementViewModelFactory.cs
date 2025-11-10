d3be9c142b1c0ffbd5b29d5231fbd899494fa723
Paul Fresquet <paul.fresquet@pow-software.com>
2025-11-10 08:32:53 +0100

    feature: synchronization confirmation popup

--- src/ByteSync.Client/Services/Misc/FlyoutElementViewModelFactory.cs
+++ src/ByteSync.Client/Services/Misc/FlyoutElementViewModelFactory.cs
@@ -3,7 +3,6 @@ using ByteSync.Business;
 using ByteSync.Business.Actions.Local;
 using ByteSync.Business.Profiles;
 using ByteSync.Common.Business.EndPoints;
-using ByteSync.Interfaces.Factories;
 using ByteSync.Interfaces.Factories.ViewModels;
 using ByteSync.Models.Comparisons.Result;
 using ByteSync.ViewModels.AccountDetails;
@@ -18,84 +17,87 @@ namespace ByteSync.Services.Misc;
 public class FlyoutElementViewModelFactory : IFlyoutElementViewModelFactory
 {
     private readonly IComponentContext _context;
-
+    
     public FlyoutElementViewModelFactory(IComponentContext context)
     {
         _context = context;
     }
     
-    public AddTrustedClientViewModel BuilAddTrustedClientViewModel(PublicKeyCheckData publicKeyCheckData, TrustDataParameters trustDataParameters)
+    public AddTrustedClientViewModel BuilAddTrustedClientViewModel(PublicKeyCheckData publicKeyCheckData,
+        TrustDataParameters trustDataParameters)
     {
         var result = _context.Resolve<AddTrustedClientViewModel>(new TypedParameter(typeof(PublicKeyCheckData), publicKeyCheckData),
             new TypedParameter(typeof(TrustDataParameters), trustDataParameters));
-
+        
         return result;
     }
-
-    public SynchronizationRuleGlobalViewModel BuilSynchronizationRuleGlobalViewModel(SynchronizationRule synchronizationRule, bool isCloneMode)
+    
+    public SynchronizationRuleGlobalViewModel BuilSynchronizationRuleGlobalViewModel(SynchronizationRule synchronizationRule,
+        bool isCloneMode)
     {
-        var result = _context.Resolve<SynchronizationRuleGlobalViewModel>(new TypedParameter(typeof(SynchronizationRule), synchronizationRule),
+        var result = _context.Resolve<SynchronizationRuleGlobalViewModel>(
+            new TypedParameter(typeof(SynchronizationRule), synchronizationRule),
             new TypedParameter(typeof(bool), isCloneMode));
-
+        
         return result;
     }
-
+    
     public CreateSessionProfileViewModel BuildCreateSessionProfileViewModel(ProfileTypes profileType)
     {
         var result = _context.Resolve<CreateSessionProfileViewModel>(new TypedParameter(typeof(ProfileTypes), profileType));
-
+        
         return result;
     }
-
+    
     public TargetedActionGlobalViewModel BuildTargetedActionGlobalViewModel(List<ComparisonItem> comparisonItems)
     {
         var result = _context.Resolve<TargetedActionGlobalViewModel>(TypedParameter.From(comparisonItems));
-
+        
         return result;
     }
-
+    
     public AccountDetailsViewModel BuildAccountDetailsViewModel()
     {
         var result = _context.Resolve<AccountDetailsViewModel>();
-
+        
         return result;
     }
-
+    
     public TrustedNetworkViewModel BuildTrustedNetworkViewModel()
     {
         var result = _context.Resolve<TrustedNetworkViewModel>();
-
+        
         return result;
     }
-
+    
     public UpdateDetailsViewModel BuildUpdateDetailsViewModel()
     {
         var result = _context.Resolve<UpdateDetailsViewModel>();
-
+        
         return result;
     }
-
+    
     public GeneralSettingsViewModel BuildGeneralSettingsViewModel()
     {
         var result = _context.Resolve<GeneralSettingsViewModel>();
-
+        
         return result;
     }
-
-    public SynchronizationRuleGlobalViewModel BuildSynchronizationRuleGlobalViewModel(SynchronizationRule? baseAutomaticAction = null, 
+    
+    public SynchronizationRuleGlobalViewModel BuildSynchronizationRuleGlobalViewModel(SynchronizationRule? baseAutomaticAction = null,
         bool isCloneMode = false)
     {
         var result = _context.Resolve<SynchronizationRuleGlobalViewModel>(
             new TypedParameter(typeof(SynchronizationRule), baseAutomaticAction),
             TypedParameter.From(isCloneMode));
-
+        
         return result;
     }
-
+    
     public FlyoutElementViewModel BuildAboutApplicationViewModel()
     {
         var result = _context.Resolve<AboutApplicationViewModel>();
-
+        
         return result;
     }
 }
