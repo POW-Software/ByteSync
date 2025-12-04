4fc5c726cedc4600809e0021d0d4e8873a2b3f4f
Paul Fresquet <paul.fresquet@pow-software.com>
2025-12-04 19:53:59 +0100

    feat: improve ThemeBuilder

--- src/ByteSync.Client/Services/Themes/ThemeBuilder.cs
+++ src/ByteSync.Client/Services/Themes/ThemeBuilder.cs
@@ -7,6 +7,8 @@ namespace ByteSync.Services.Themes;
 
 public class ThemeBuilder : IThemeBuilder
 {
+    private const double Epsilon = 0.0001;
+    
     private record ColorSchemeConfig(
         double MainAccentSaturation,
         double MainAccentValue,
@@ -46,6 +48,62 @@ public class ThemeBuilder : IThemeBuilder
         Color MainWindowBottomColor,
         bool UseDarkAccents);
     
+    private static readonly ColorSchemeConfig DarkConfig = new(
+        MainAccentSaturation: 0.65, MainAccentValue: 0.50,
+        AccentTextSaturation: 0.33, AccentTextValue: 0.85,
+        HomeCloudSyncSaturation: 0.55, HomeCloudSyncValue: 0.70,
+        HomeCloudSyncPointerOverSaturation: 0.25, HomeCloudSyncPointerOverValue: 0.50,
+        ChartsBarSaturation: 0.50, ChartsBarValue: 0.75,
+        CurrentMemberSaturation: 0.35, CurrentMemberValue: 0.22,
+        ConnectedMemberLetterSaturation: 0.35, ConnectedMemberLetterValue: 0.28,
+        ConnectedMemberLetterBorderSaturation: 0.35, ConnectedMemberLetterBorderValue: 0.34,
+        BsAccentButtonSaturation: 0.55, BsAccentButtonValue: 0.25,
+        BsAccentButtonPointerOverSaturation: 0.55, BsAccentButtonPointerOverValue: 0.35,
+        StatusMainBackGroundSaturation: 0.45, StatusMainBackGroundValue: 0.25,
+        DisabledMemberBackGround: new Color(0xFF, 0x30, 0x30, 0x30),
+        DisabledMemberLetterBackGround: new Color(0xFF, 0x37, 0x37, 0x37),
+        DisabledMemberLetterBorder: new Color(0xFF, 0x3D, 0x3D, 0x3D),
+        VeryLightGray: new Color(0xFF, 0x12, 0x12, 0x12),
+        GenericButtonBorder: new Color(0xFF, 0x55, 0x55, 0x55),
+        Gray1: new Color(0xFF, 0xCC, 0xCC, 0xCC),
+        Gray2: new Color(0xFF, 0x80, 0x80, 0x80),
+        Gray5: new Color(0xFF, 0x46, 0x46, 0x46),
+        Gray7: new Color(0xFF, 0x3A, 0x3A, 0x3A),
+        Gray8: new Color(0xFF, 0x2C, 0x2C, 0x2C),
+        SettingsHeaderColor: new Color(0xFF, 0x30, 0x30, 0x30),
+        BlockBackColor: new Color(0xFF, 0x1F, 0x1F, 0x1F),
+        MainWindowTopColor: new Color(0xFF, 0x12, 0x12, 0x12),
+        MainWindowBottomColor: new Color(0xFF, 0x04, 0x04, 0x04),
+        UseDarkAccents: true);
+    
+    private static readonly ColorSchemeConfig LightConfig = new(
+        MainAccentSaturation: 1.0, MainAccentValue: 1.0,
+        AccentTextSaturation: 1.0, AccentTextValue: 1.0,
+        HomeCloudSyncSaturation: 0.50, HomeCloudSyncValue: 0.65,
+        HomeCloudSyncPointerOverSaturation: 0.25, HomeCloudSyncPointerOverValue: 0.55,
+        ChartsBarSaturation: 0.50, ChartsBarValue: 0.80,
+        CurrentMemberSaturation: 0.20, CurrentMemberValue: 0.92,
+        ConnectedMemberLetterSaturation: 0.20, ConnectedMemberLetterValue: 0.84,
+        ConnectedMemberLetterBorderSaturation: 0.20, ConnectedMemberLetterBorderValue: 0.78,
+        BsAccentButtonSaturation: 0.15, BsAccentButtonValue: 0.95,
+        BsAccentButtonPointerOverSaturation: 0.12, BsAccentButtonPointerOverValue: 0.98,
+        StatusMainBackGroundSaturation: 0.35, StatusMainBackGroundValue: 0.90,
+        DisabledMemberBackGround: new Color(0xFF, 0xEC, 0xEC, 0xEC),
+        DisabledMemberLetterBackGround: new Color(0xFF, 0xE6, 0xE6, 0xE6),
+        DisabledMemberLetterBorder: new Color(0xFF, 0xE0, 0xE0, 0xE0),
+        VeryLightGray: new Color(0xFF, 0xF7, 0xF7, 0xF7),
+        GenericButtonBorder: new Color(0xFF, 0xAA, 0xAA, 0xAA),
+        Gray1: new Color(0xFF, 0x33, 0x33, 0x33),
+        Gray2: new Color(0xFF, 0x7F, 0x7F, 0x7F),
+        Gray5: new Color(0xFF, 0xB9, 0xB9, 0xB9),
+        Gray7: new Color(0xFF, 0xD6, 0xD6, 0xD6),
+        Gray8: new Color(0xFF, 0xE0, 0xE0, 0xE0),
+        SettingsHeaderColor: new Color(0xFF, 0xEC, 0xEC, 0xEC),
+        BlockBackColor: new Color(0xFF, 0xFF, 0xFF, 0xFF),
+        MainWindowTopColor: new Color(0xFF, 0xFA, 0xFA, 0xFA),
+        MainWindowBottomColor: new Color(0xFF, 0xEF, 0xEF, 0xEF),
+        UseDarkAccents: false);
+    
     public Theme BuildTheme(string themeName, string primaryColorHex, ThemeModes themeMode)
     {
         return BuildTheme(themeName, primaryColorHex, themeMode, -60);
@@ -72,16 +130,9 @@ public class ThemeBuilder : IThemeBuilder
     private ColorScheme BuildColorScheme(ThemeColor themeColor, double secondaryColorHue, ThemeModes themeMode)
     {
         var colorScheme = new ColorScheme(themeMode);
+        var config = themeMode == ThemeModes.Dark ? DarkConfig : LightConfig;
         
-        if (themeMode == ThemeModes.Dark)
-        {
-            BuildDarkColorScheme(colorScheme, themeColor, secondaryColorHue);
-        }
-        else
-        {
-            BuildLightColorScheme(colorScheme, themeColor, secondaryColorHue);
-        }
-        
+        ApplyColorSchemeConfig(colorScheme, themeColor, secondaryColorHue, config);
         ComputeSecondaryColors(colorScheme, secondaryColorHue, themeMode);
         
         colorScheme.StatusMainBackGroundBrush = new SolidColorBrush(colorScheme.StatusMainBackGround.AvaloniaColor);
@@ -93,104 +144,18 @@ public class ThemeBuilder : IThemeBuilder
         return colorScheme;
     }
     
-    private void BuildDarkColorScheme(ColorScheme colorScheme, ThemeColor themeColor, double secondaryColorHue)
+    private void ApplyColorSchemeConfig(ColorScheme colorScheme, ThemeColor themeColor, double secondaryColorHue, ColorSchemeConfig config)
     {
-        var config = new ColorSchemeConfig(
-            MainAccentSaturation: 0.65,
-            MainAccentValue: 0.50,
-            AccentTextSaturation: 0.33,
-            AccentTextValue: 0.85,
-            HomeCloudSyncSaturation: 0.55,
-            HomeCloudSyncValue: 0.70,
-            HomeCloudSyncPointerOverSaturation: 0.25,
-            HomeCloudSyncPointerOverValue: 0.50,
-            ChartsBarSaturation: 0.50,
-            ChartsBarValue: 0.75,
-            CurrentMemberSaturation: 0.35,
-            CurrentMemberValue: 0.22,
-            ConnectedMemberLetterSaturation: 0.35,
-            ConnectedMemberLetterValue: 0.28,
-            ConnectedMemberLetterBorderSaturation: 0.35,
-            ConnectedMemberLetterBorderValue: 0.34,
-            BsAccentButtonSaturation: 0.55,
-            BsAccentButtonValue: 0.25,
-            BsAccentButtonPointerOverSaturation: 0.55,
-            BsAccentButtonPointerOverValue: 0.35,
-            StatusMainBackGroundSaturation: 0.45,
-            StatusMainBackGroundValue: 0.25,
-            DisabledMemberBackGround: Color.FromArgb(0xFF, 0x30, 0x30, 0x30),
-            DisabledMemberLetterBackGround: Color.FromArgb(0xFF, 0x37, 0x37, 0x37),
-            DisabledMemberLetterBorder: Color.FromArgb(0xFF, 0x3D, 0x3D, 0x3D),
-            VeryLightGray: Color.FromArgb(0xFF, 0x12, 0x12, 0x12),
-            GenericButtonBorder: Color.FromArgb(0xFF, 0x55, 0x55, 0x55),
-            Gray1: Color.FromArgb(0xFF, 0xCC, 0xCC, 0xCC),
-            Gray2: Color.FromArgb(0xFF, 0x80, 0x80, 0x80),
-            Gray5: Color.FromArgb(0xFF, 0x46, 0x46, 0x46),
-            Gray7: Color.FromArgb(0xFF, 0x3A, 0x3A, 0x3A),
-            Gray8: Color.FromArgb(0xFF, 0x2C, 0x2C, 0x2C),
-            SettingsHeaderColor: Color.FromArgb(0xFF, 0x30, 0x30, 0x30),
-            BlockBackColor: Color.FromArgb(0xFF, 0x1F, 0x1F, 0x1F),
-            MainWindowTopColor: Color.FromArgb(0xFF, 0x12, 0x12, 0x12),
-            MainWindowBottomColor: Color.FromArgb(0xFF, 0x04, 0x04, 0x04),
-            UseDarkAccents: true);
-        
-        BuildColorScheme(colorScheme, themeColor, secondaryColorHue, config);
-    }
-    
-    private void BuildLightColorScheme(ColorScheme colorScheme, ThemeColor themeColor, double secondaryColorHue)
-    {
-        var config = new ColorSchemeConfig(
-            MainAccentSaturation: 1.0,
-            MainAccentValue: 1.0,
-            AccentTextSaturation: 1.0,
-            AccentTextValue: 1.0,
-            HomeCloudSyncSaturation: 0.50,
-            HomeCloudSyncValue: 0.65,
-            HomeCloudSyncPointerOverSaturation: 0.25,
-            HomeCloudSyncPointerOverValue: 0.55,
-            ChartsBarSaturation: 0.50,
-            ChartsBarValue: 0.80,
-            CurrentMemberSaturation: 0.20,
-            CurrentMemberValue: 0.92,
-            ConnectedMemberLetterSaturation: 0.20,
-            ConnectedMemberLetterValue: 0.84,
-            ConnectedMemberLetterBorderSaturation: 0.20,
-            ConnectedMemberLetterBorderValue: 0.78,
-            BsAccentButtonSaturation: 0.15,
-            BsAccentButtonValue: 0.95,
-            BsAccentButtonPointerOverSaturation: 0.12,
-            BsAccentButtonPointerOverValue: 0.98,
-            StatusMainBackGroundSaturation: 0.35,
-            StatusMainBackGroundValue: 0.90,
-            DisabledMemberBackGround: Color.FromArgb(0xFF, 0xEC, 0xEC, 0xEC),
-            DisabledMemberLetterBackGround: Color.FromArgb(0xFF, 0xE6, 0xE6, 0xE6),
-            DisabledMemberLetterBorder: Color.FromArgb(0xFF, 0xE0, 0xE0, 0xE0),
-            VeryLightGray: Color.FromArgb(0xFF, 0xF7, 0xF7, 0xF7),
-            GenericButtonBorder: Color.FromArgb(0xFF, 0xAA, 0xAA, 0xAA),
-            Gray1: Color.FromArgb(0xFF, 0x33, 0x33, 0x33),
-            Gray2: Color.FromArgb(0xFF, 0x7F, 0x7F, 0x7F),
-            Gray5: Color.FromArgb(0xFF, 0xB9, 0xB9, 0xB9),
-            Gray7: Color.FromArgb(0xFF, 0xD6, 0xD6, 0xD6),
-            Gray8: Color.FromArgb(0xFF, 0xE0, 0xE0, 0xE0),
-            SettingsHeaderColor: Color.FromArgb(0xFF, 0xEC, 0xEC, 0xEC),
-            BlockBackColor: Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF),
-            MainWindowTopColor: Color.FromArgb(0xFF, 0xFA, 0xFA, 0xFA),
-            MainWindowBottomColor: Color.FromArgb(0xFF, 0xEF, 0xEF, 0xEF),
-            UseDarkAccents: false);
-        
-        BuildColorScheme(colorScheme, themeColor, secondaryColorHue, config);
-    }
-    
-    private void BuildColorScheme(ColorScheme colorScheme, ThemeColor themeColor, double secondaryColorHue, ColorSchemeConfig config)
-    {
-        colorScheme.MainAccentColor = config.MainAccentSaturation == 1.0 && config.MainAccentValue == 1.0
-            ? themeColor
-            : themeColor.WithSaturationValue(config.MainAccentSaturation, config.MainAccentValue);
+        colorScheme.MainAccentColor =
+            Math.Abs(config.MainAccentSaturation - 1.0) < Epsilon && Math.Abs(config.MainAccentValue - 1.0) < Epsilon
+                ? themeColor
+                : themeColor.WithSaturationValue(config.MainAccentSaturation, config.MainAccentValue);
         colorScheme.MainSecondaryColor = colorScheme.MainAccentColor.WithHue(secondaryColorHue);
         
-        colorScheme.AccentTextForeGround = config.AccentTextSaturation == 1.0 && config.AccentTextValue == 1.0
-            ? themeColor.AvaloniaColor
-            : themeColor.WithSaturationValue(config.AccentTextSaturation, config.AccentTextValue).AvaloniaColor;
+        colorScheme.AccentTextForeGround =
+            Math.Abs(config.AccentTextSaturation - 1.0) < Epsilon && Math.Abs(config.AccentTextValue - 1.0) < Epsilon
+                ? themeColor.AvaloniaColor
+                : themeColor.WithSaturationValue(config.AccentTextSaturation, config.AccentTextValue).AvaloniaColor;
         
         colorScheme.HomeCloudSynchronizationBackGround = themeColor
             .WithSaturationValue(config.HomeCloudSyncSaturation, config.HomeCloudSyncValue).AvaloniaColor;
@@ -234,23 +199,7 @@ public class ThemeBuilder : IThemeBuilder
             .WithSaturationValue(config.StatusMainBackGroundSaturation, config.StatusMainBackGroundValue);
         
         ComputeAttenuations(colorScheme);
-        
-        if (config.UseDarkAccents)
-        {
-            colorScheme.Accent1 = colorScheme.SystemAccentColorDark1.AvaloniaColor;
-            colorScheme.Accent2 = colorScheme.SystemAccentColorDark2.AvaloniaColor;
-            colorScheme.Accent3 = colorScheme.SystemAccentColorDark3.AvaloniaColor;
-            colorScheme.Accent4 = colorScheme.SystemAccentColorDark4.AvaloniaColor;
-            colorScheme.Accent5 = colorScheme.SystemAccentColorDark5.AvaloniaColor;
-        }
-        else
-        {
-            colorScheme.Accent1 = colorScheme.SystemAccentColorLight1.AvaloniaColor;
-            colorScheme.Accent2 = colorScheme.SystemAccentColorLight2.AvaloniaColor;
-            colorScheme.Accent3 = colorScheme.SystemAccentColorLight3.AvaloniaColor;
-            colorScheme.Accent4 = colorScheme.SystemAccentColorLight4.AvaloniaColor;
-            colorScheme.Accent5 = colorScheme.SystemAccentColorLight5.AvaloniaColor;
-        }
+        AssignAccentColors(colorScheme, config.UseDarkAccents);
         
         colorScheme.VeryLightGray = config.VeryLightGray;
         colorScheme.GenericButtonBorder = config.GenericButtonBorder;
@@ -296,16 +245,42 @@ public class ThemeBuilder : IThemeBuilder
     
     private static void ComputeAttenuations(ColorScheme colorScheme)
     {
-        colorScheme.SystemAccentColorDark1 = colorScheme.MainAccentColor.AdjustSaturationValue(0, -0.10);
-        colorScheme.SystemAccentColorDark2 = colorScheme.MainAccentColor.AdjustSaturationValue(0, -0.20);
-        colorScheme.SystemAccentColorDark3 = colorScheme.MainAccentColor.AdjustSaturationValue(0, -0.30);
-        colorScheme.SystemAccentColorDark4 = colorScheme.MainAccentColor.AdjustSaturationValue(0, -0.40);
-        colorScheme.SystemAccentColorDark5 = colorScheme.MainAccentColor.AdjustSaturationValue(0, -0.50);
+        const double step = 0.10;
+        var darkColors = new ThemeColor[5];
+        var lightColors = new ThemeColor[5];
         
-        colorScheme.SystemAccentColorLight1 = colorScheme.MainAccentColor.AdjustSaturationValue(0, 0.10);
-        colorScheme.SystemAccentColorLight2 = colorScheme.MainAccentColor.AdjustSaturationValue(0, 0.20);
-        colorScheme.SystemAccentColorLight3 = colorScheme.MainAccentColor.AdjustSaturationValue(0, 0.30);
-        colorScheme.SystemAccentColorLight4 = colorScheme.MainAccentColor.AdjustSaturationValue(0, 0.40);
-        colorScheme.SystemAccentColorLight5 = colorScheme.MainAccentColor.AdjustSaturationValue(0, 0.50);
+        for (int i = 0; i < 5; i++)
+        {
+            var offset = step * (i + 1);
+            darkColors[i] = colorScheme.MainAccentColor.AdjustSaturationValue(0, -offset);
+            lightColors[i] = colorScheme.MainAccentColor.AdjustSaturationValue(0, offset);
+        }
+        
+        colorScheme.SystemAccentColorDark1 = darkColors[0];
+        colorScheme.SystemAccentColorDark2 = darkColors[1];
+        colorScheme.SystemAccentColorDark3 = darkColors[2];
+        colorScheme.SystemAccentColorDark4 = darkColors[3];
+        colorScheme.SystemAccentColorDark5 = darkColors[4];
+        
+        colorScheme.SystemAccentColorLight1 = lightColors[0];
+        colorScheme.SystemAccentColorLight2 = lightColors[1];
+        colorScheme.SystemAccentColorLight3 = lightColors[2];
+        colorScheme.SystemAccentColorLight4 = lightColors[3];
+        colorScheme.SystemAccentColorLight5 = lightColors[4];
+    }
+    
+    private static void AssignAccentColors(ColorScheme colorScheme, bool useDarkAccents)
+    {
+        var source1 = useDarkAccents ? colorScheme.SystemAccentColorDark1 : colorScheme.SystemAccentColorLight1;
+        var source2 = useDarkAccents ? colorScheme.SystemAccentColorDark2 : colorScheme.SystemAccentColorLight2;
+        var source3 = useDarkAccents ? colorScheme.SystemAccentColorDark3 : colorScheme.SystemAccentColorLight3;
+        var source4 = useDarkAccents ? colorScheme.SystemAccentColorDark4 : colorScheme.SystemAccentColorLight4;
+        var source5 = useDarkAccents ? colorScheme.SystemAccentColorDark5 : colorScheme.SystemAccentColorLight5;
+        
+        colorScheme.Accent1 = source1.AvaloniaColor;
+        colorScheme.Accent2 = source2.AvaloniaColor;
+        colorScheme.Accent3 = source3.AvaloniaColor;
+        colorScheme.Accent4 = source4.AvaloniaColor;
+        colorScheme.Accent5 = source5.AvaloniaColor;
     }
 }
