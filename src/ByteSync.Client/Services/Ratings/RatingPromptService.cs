a0c0f30747b0588e5f773f13b23b69576383ac4a
Paul Fresquet <paul.fresquet@pow-software.com>
2026-01-04 11:33:13 +0100

    refactor: fix sonar feedbacks

--- src/ByteSync.Client/Services/Ratings/RatingPromptService.cs
+++ src/ByteSync.Client/Services/Ratings/RatingPromptService.cs
@@ -18,12 +18,21 @@ public class RatingPromptService : IRatingPromptService, IDisposable
 {
     internal const double PROMPT_PROBABILITY = 1d / 3d;
     
+#pragma warning disable S1075
     private const string STORE_RATING_URL = "https://apps.microsoft.com/detail/9p17gqw3z2q2?hl=fr-FR&gl=FR";
+    private const string GITHUB_RATING_URL = "https://github.com/POW-Software/ByteSync";
+    private const string ALTERNATIVETO_RATING_URL = "https://alternativeto.net/software/bytesync/about/";
+    private const string MAJOR_GEEKS_RATING_URL = "https://www.majorgeeks.com/files/details/bytesync.html";
+    private const string SOFTPEDIA_RATING_URL = "https://www.softpedia.com/get/System/Back-Up-and-Recovery/ByteSync.shtml";
+    private const string UPTODOWN_RATING_URL = "https://bytesync-windows.fr.uptodown.com/windows";
+    private const string SOURCE_FORGE_RATING_URL = "https://sourceforge.net/projects/bytesync/";
+#pragma warning restore S1075
     
     private const int RANDOM_ADDITIONAL_OPTIONS_COUNT = 3;
     private const string RATING_PROMPT_CHANNEL_SOFTPEDIA_KEY = "RatingPrompt_Channel_Softpedia";
     private const string RATING_PROMPT_CHANNEL_UPTODOWN_KEY = "RatingPrompt_Channel_Uptodown";
     private const string RATING_PROMPT_CHANNEL_SOURCE_FORGE_KEY = "RatingPrompt_Channel_SourceForge";
+    private const string REGULAR_WORLD_ICON = "RegularWorld";
     
     private static readonly RatingChannel StoreRatingChannel = new(
         nameof(Resources.RatingPrompt_Channel_MicrosoftStore),
@@ -32,31 +41,31 @@ public class RatingPromptService : IRatingPromptService, IDisposable
     
     private static readonly RatingChannel GitHubRatingChannel = new(
         nameof(Resources.RatingPrompt_Channel_GitHub),
-        "https://github.com/POW-Software/ByteSync",
+        GITHUB_RATING_URL,
         "LogosGithub");
     
-    private static readonly IReadOnlyList<RatingChannel> AdditionalRatingChannels = new[]
+    private static readonly RatingChannel[] AdditionalRatingChannels =
     {
         new RatingChannel(
             nameof(Resources.RatingPrompt_Channel_AlternativeTo),
-            "https://alternativeto.net/software/bytesync/about/",
-            "RegularWorld"),
+            ALTERNATIVETO_RATING_URL,
+            REGULAR_WORLD_ICON),
         new RatingChannel(
             nameof(Resources.RatingPrompt_Channel_MajorGeeks),
-            "https://www.majorgeeks.com/files/details/bytesync.html",
-            "RegularWorld"),
+            MAJOR_GEEKS_RATING_URL,
+            REGULAR_WORLD_ICON),
         new RatingChannel(
             RATING_PROMPT_CHANNEL_SOFTPEDIA_KEY,
-            "https://www.softpedia.com/get/System/Back-Up-and-Recovery/ByteSync.shtml",
-            "RegularWorld"),
+            SOFTPEDIA_RATING_URL,
+            REGULAR_WORLD_ICON),
         new RatingChannel(
             RATING_PROMPT_CHANNEL_UPTODOWN_KEY,
-            "https://bytesync-windows.fr.uptodown.com/windows",
-            "RegularWorld"),
+            UPTODOWN_RATING_URL,
+            REGULAR_WORLD_ICON),
         new RatingChannel(
             RATING_PROMPT_CHANNEL_SOURCE_FORGE_KEY,
-            "https://sourceforge.net/projects/bytesync/",
-            "RegularWorld")
+            SOURCE_FORGE_RATING_URL,
+            REGULAR_WORLD_ICON)
     };
     
     private readonly ISynchronizationService _synchronizationService;
@@ -177,7 +186,7 @@ public class RatingPromptService : IRatingPromptService, IDisposable
         }
     }
     
-    private IEnumerable<RatingOption> BuildRatingOptions()
+    private List<RatingOption> BuildRatingOptions()
     {
         var options = new List<RatingOption>();
         
@@ -191,7 +200,7 @@ public class RatingPromptService : IRatingPromptService, IDisposable
             options.Add(CreateRatingOption(GitHubRatingChannel));
             
             var additionalChannels = GetRandomizedChannels(AdditionalRatingChannels)
-                .Take(Math.Min(RANDOM_ADDITIONAL_OPTIONS_COUNT, AdditionalRatingChannels.Count));
+                .Take(Math.Min(RANDOM_ADDITIONAL_OPTIONS_COUNT, AdditionalRatingChannels.Length));
             foreach (var channel in additionalChannels)
             {
                 options.Add(CreateRatingOption(channel));
