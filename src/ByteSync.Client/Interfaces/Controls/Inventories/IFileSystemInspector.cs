0a96d99acd1a49255e2570d3de396736bc6f72b5
Paul Fresquet <paul.fresquet@pow-software.com>
2026-02-10 14:04:30 +0100

    refactor: cleanup

--- src/ByteSync.Client/Interfaces/Controls/Inventories/IFileSystemInspector.cs
+++ src/ByteSync.Client/Interfaces/Controls/Inventories/IFileSystemInspector.cs
@@ -7,11 +7,18 @@ namespace ByteSync.Interfaces.Controls.Inventories;
 public interface IFileSystemInspector
 {
     FileSystemEntryKind ClassifyEntry(FileSystemInfo fsi);
+    
     bool IsHidden(FileSystemInfo fsi, OSPlatforms os);
+    
     bool IsSystemAttribute(FileInfo fileInfo);
+    
     bool IsNoiseFileName(FileInfo fileInfo, OSPlatforms os);
+    
     bool IsReparsePoint(FileSystemInfo fsi);
+    
     bool Exists(FileInfo fileInfo);
+    
     bool IsOffline(FileInfo fileInfo);
+    
     bool IsRecallOnDataAccess(FileInfo fileInfo);
 }
