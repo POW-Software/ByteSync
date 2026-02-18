fb1a88f3f8f58bd50770fdfcf174554315a1b9e4
Paul Fresquet <paul.fresquet@pow-software.com>
2026-02-18 17:06:59 +0100

    refactor: cleanup

--- src/ByteSync.Client/Models/Inventories/InventoryPart.cs
+++ src/ByteSync.Client/Models/Inventories/InventoryPart.cs
@@ -11,38 +11,32 @@ public class InventoryPart
         FileDescriptions = new List<FileDescription>();
         DirectoryDescriptions = new List<DirectoryDescription>();
     }
-
+    
     public InventoryPart(Inventory inventory, string rootPath, FileSystemTypes inventoryPartType) : this()
     {
         Inventory = inventory;
         RootPath = rootPath;
         InventoryPartType = inventoryPartType;
     }
-
+    
     public Inventory Inventory { get; set; }
-        
+    
     public string RootPath { get; set; }
-
+    
     public FileSystemTypes InventoryPartType { get; set; }
-        
+    
     public string Code { get; set; }
-
+    
     public List<FileDescription> FileDescriptions { get; set; }
-
+    
     public List<DirectoryDescription> DirectoryDescriptions { get; set; }
-
+    
     public bool IsIncompleteDueToAccess { get; set; }
     
-    private Dictionary<SkipReason, int> _skippedCountsByReason = new();
-    
-    public Dictionary<SkipReason, int> SkippedCountsByReason
-    {
-        get => _skippedCountsByReason;
-        set => _skippedCountsByReason = value ?? new Dictionary<SkipReason, int>();
-    }
+    public Dictionary<SkipReason, int> SkippedCountsByReason { get; set; } = new Dictionary<SkipReason, int>();
     
     public int SkippedCount => SkippedCountsByReason.Values.Sum();
-
+    
     public string RootName
     {
         get
@@ -52,63 +46,67 @@ public class InventoryPart
             {
                 case OSPlatforms.Windows:
                     directorySeparatorChar = "\\";
+                    
                     break;
                 case OSPlatforms.Linux:
                 case OSPlatforms.MacOs:
                     directorySeparatorChar = "/";
+                    
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(directorySeparatorChar));
             }
-                
+            
             return RootPath.Substring(RootPath.LastIndexOf(directorySeparatorChar, StringComparison.Ordinal));
         }
     }
-
+    
     protected bool Equals(InventoryPart other)
     {
         return Equals(Inventory, other.Inventory) && RootPath == other.RootPath && InventoryPartType == other.InventoryPartType;
     }
-
+    
     public override bool Equals(object obj)
     {
         if (ReferenceEquals(null, obj)) return false;
         if (ReferenceEquals(this, obj)) return true;
         if (obj.GetType() != this.GetType()) return false;
-        return Equals((InventoryPart) obj);
+        
+        return Equals((InventoryPart)obj);
     }
-
+    
     public override int GetHashCode()
     {
         unchecked
         {
             var hashCode = Inventory.GetHashCode();
             hashCode = (hashCode * 397) ^ RootPath.GetHashCode();
-            hashCode = (hashCode * 397) ^ (int) InventoryPartType;
+            hashCode = (hashCode * 397) ^ (int)InventoryPartType;
+            
             return hashCode;
         }
     }
-
+    
     public override string ToString()
     {
     #if DEBUG
         return $"InventoryPart {RootName} {RootPath}";
     #endif
-
+        
 #pragma warning disable 162
         return base.ToString();
 #pragma warning restore 162
     }
-
+    
     public void AddFileSystemDescription(FileSystemDescription fileSystemDescription)
     {
         if (fileSystemDescription.FileSystemType == FileSystemTypes.File)
         {
-            FileDescriptions.Add((FileDescription) fileSystemDescription);
+            FileDescriptions.Add((FileDescription)fileSystemDescription);
         }
         else
         {
-            DirectoryDescriptions.Add((DirectoryDescription) fileSystemDescription);
+            DirectoryDescriptions.Add((DirectoryDescription)fileSystemDescription);
         }
     }
     
