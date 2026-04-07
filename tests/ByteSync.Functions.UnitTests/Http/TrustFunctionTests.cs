bb221f94cbcc19ab0857dfa7ab6be972ebb5018c
NETTOUR Marwan <marwan.nettour@epitech.eu>
2026-04-07 06:07:49 -0400

    [test] Update TrustFunctionTests for .NET 10 migration

--- tests/ByteSync.Functions.UnitTests/Http/TrustFunctionTests.cs
+++ tests/ByteSync.Functions.UnitTests/Http/TrustFunctionTests.cs
@@ -2,8 +2,10 @@ using System.Net;
 using System.Text;
 using ByteSync.Common.Business.Misc;
 using ByteSync.Common.Business.Sessions.Cloud.Connections;
+using ByteSync.Common.Business.Trust.Connections;
 using ByteSync.Common.Business.Versions;
 using ByteSync.Common.Controls.Json;
+using ByteSync.Common.Business.EndPoints;
 using ByteSync.Functions.Http;
 using ByteSync.Functions.UnitTests.TestHelpers;
 using ByteSync.ServerCommon.Business.Auth;
@@ -30,6 +32,159 @@ public class TrustFunctionTests
         return mockContext.Object;
     }
     
+    private static async Task WriteBodyAsync<T>(FakeHttpRequestData request, T body)
+    {
+        var json = JsonHelper.Serialize(body);
+        var bytes = Encoding.UTF8.GetBytes(json);
+        request.Body.SetLength(0);
+        await request.Body.WriteAsync(bytes, 0, bytes.Length);
+        request.Body.Position = 0;
+    }
+    
+    [Test]
+    public async Task StartTrustCheck_ForwardsRequest_AndReturnsOk()
+    {
+        var mediatorMock = new Mock<IMediator>();
+        StartTrustCheckRequest? captured = null;
+        mediatorMock
+            .Setup(m => m.Send(It.IsAny<StartTrustCheckRequest>(), It.IsAny<CancellationToken>()))
+            .Callback<object, CancellationToken>((r, _) => captured = (StartTrustCheckRequest)r)
+            .ReturnsAsync(new StartTrustCheckResult { IsOK = true });
+        
+        var function = new TrustFunction(mediatorMock.Object);
+        var context = BuildFunctionContextWithClient();
+        var request = new FakeHttpRequestData(context);
+        
+        var parameters = new TrustCheckParameters { SessionId = "S1", MembersInstanceIdsToCheck = ["VI"] };
+        await WriteBodyAsync(request, parameters);
+        
+        var response = await function.StartTrustCheck(request, context);
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        captured.Should().NotBeNull();
+        captured!.Parameters.SessionId.Should().Be("S1");
+    }
+
+    [Test]
+    public async Task GiveMemberPublicKeyCheckData_ForwardsRequest_AndReturnsOk()
+    {
+        var mediatorMock = new Mock<IMediator>();
+        GiveMemberPublicKeyCheckDataRequest? captured = null;
+        mediatorMock
+            .Setup(m => m.Send(It.IsAny<GiveMemberPublicKeyCheckDataRequest>(), It.IsAny<CancellationToken>()))
+            .Callback<object, CancellationToken>((r, _) => captured = (GiveMemberPublicKeyCheckDataRequest)r)
+            .Returns(Task.CompletedTask);
+        
+        var function = new TrustFunction(mediatorMock.Object);
+        var context = BuildFunctionContextWithClient();
+        var request = new FakeHttpRequestData(context);
+        
+        var parameters = new GiveMemberPublicKeyCheckDataParameters { SessionId = "S1", PublicKeyCheckData = new PublicKeyCheckData() };
+        await WriteBodyAsync(request, parameters);
+        
+        var response = await function.GiveMemberPublicKeyCheckData(request, context);
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        captured.Should().NotBeNull();
+        captured!.Parameters.SessionId.Should().Be("S1");
+    }
+
+    [Test]
+    public async Task SendDigitalSignatures_ForwardsRequest_AndReturnsOk()
+    {
+        var mediatorMock = new Mock<IMediator>();
+        SendDigitalSignaturesRequest? captured = null;
+        mediatorMock
+            .Setup(m => m.Send(It.IsAny<SendDigitalSignaturesRequest>(), It.IsAny<CancellationToken>()))
+            .Callback<object, CancellationToken>((r, _) => captured = (SendDigitalSignaturesRequest)r)
+            .Returns(Task.CompletedTask);
+        
+        var function = new TrustFunction(mediatorMock.Object);
+        var context = BuildFunctionContextWithClient();
+        var request = new FakeHttpRequestData(context);
+        
+        var parameters = new SendDigitalSignaturesParameters { DataId = "S1", DigitalSignatureCheckInfos = new List<DigitalSignatureCheckInfo>() };
+        await WriteBodyAsync(request, parameters);
+        
+        var response = await function.SendDigitalSignatures(request, context);
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        captured.Should().NotBeNull();
+        captured!.Parameters.DataId.Should().Be("S1");
+    }
+
+    [Test]
+    public async Task SetAuthChecked_ForwardsRequest_AndReturnsOk()
+    {
+        var mediatorMock = new Mock<IMediator>();
+        SetAuthCheckedRequest? captured = null;
+        mediatorMock
+            .Setup(m => m.Send(It.IsAny<SetAuthCheckedRequest>(), It.IsAny<CancellationToken>()))
+            .Callback<object, CancellationToken>((r, _) => captured = (SetAuthCheckedRequest)r)
+            .Returns(Task.CompletedTask);
+        
+        var function = new TrustFunction(mediatorMock.Object);
+        var context = BuildFunctionContextWithClient();
+        var request = new FakeHttpRequestData(context);
+        
+        var parameters = new SetAuthCheckedParameters { SessionId = "S1" };
+        await WriteBodyAsync(request, parameters);
+        
+        var response = await function.SetAuthChecked(request, context);
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        captured.Should().NotBeNull();
+        captured!.Parameters.SessionId.Should().Be("S1");
+    }
+
+    [Test]
+    public async Task RequestTrustPublicKey_ForwardsRequest_AndReturnsOk()
+    {
+        var mediatorMock = new Mock<IMediator>();
+        RequestTrustPublicKeyRequest? captured = null;
+        mediatorMock
+            .Setup(m => m.Send(It.IsAny<RequestTrustPublicKeyRequest>(), It.IsAny<CancellationToken>()))
+            .Callback<object, CancellationToken>((r, _) => captured = (RequestTrustPublicKeyRequest)r)
+            .Returns(Task.CompletedTask);
+        
+        var function = new TrustFunction(mediatorMock.Object);
+        var context = BuildFunctionContextWithClient();
+        var request = new FakeHttpRequestData(context);
+        
+        var parameters = new RequestTrustProcessParameters { SessionId = "S1" };
+        await WriteBodyAsync(request, parameters);
+        
+        var response = await function.RequestTrustPublicKey(request, context);
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        captured.Should().NotBeNull();
+        captured!.Parameters.SessionId.Should().Be("S1");
+    }
+
+    [Test]
+    public async Task InformPublicKeyValidationIsFinished_ForwardsRequest_AndReturnsOk()
+    {
+        var mediatorMock = new Mock<IMediator>();
+        InformPublicKeyValidationIsFinishedRequest? captured = null;
+        mediatorMock
+            .Setup(m => m.Send(It.IsAny<InformPublicKeyValidationIsFinishedRequest>(), It.IsAny<CancellationToken>()))
+            .Callback<object, CancellationToken>((r, _) => captured = (InformPublicKeyValidationIsFinishedRequest)r)
+            .Returns(Task.CompletedTask);
+        
+        var function = new TrustFunction(mediatorMock.Object);
+        var context = BuildFunctionContextWithClient();
+        var request = new FakeHttpRequestData(context);
+        
+        var parameters = new PublicKeyValidationParameters { SessionId = "S1" };
+        await WriteBodyAsync(request, parameters);
+        
+        var response = await function.InformPublicKeyValidationIsFinished(request, context);
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        captured.Should().NotBeNull();
+        captured!.Parameters.SessionId.Should().Be("S1");
+    }
+    
     [Test]
     public async Task InformProtocolVersionIncompatible_ForwardsRequest_AndReturnsOk()
     {
@@ -42,9 +197,9 @@ public class TrustFunctionTests
         
         var function = new TrustFunction(mediatorMock.Object);
         var context = BuildFunctionContextWithClient();
-        
         var request = new FakeHttpRequestData(context);
-        var body = new InformProtocolVersionIncompatibleParameters
+        
+        var parameters = new InformProtocolVersionIncompatibleParameters
         {
             SessionId = "test-session-id",
             MemberClientInstanceId = "member-instance-id",
@@ -52,13 +207,7 @@ public class TrustFunctionTests
             MemberProtocolVersion = ProtocolVersion.CURRENT,
             JoinerProtocolVersion = 0
         };
-        var json = JsonHelper.Serialize(body);
-        await using (var writer = new StreamWriter(request.Body, Encoding.UTF8, 1024, leaveOpen: true))
-        {
-            await writer.WriteAsync(json);
-        }
-        
-        request.Body.Position = 0;
+        await WriteBodyAsync(request, parameters);
         
         var response = await function.InformProtocolVersionIncompatible(request, context);
         
